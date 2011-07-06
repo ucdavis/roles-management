f9308997effd8ab401e090e54aa9ffd1c689aeed
Christopher Thielen <cthielen@gmail.com>
2011-07-06 11:55:19 -0700

    Added highlighting on nav menu.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def current_controller?(options)
+      url_string = CGI.escapeHTML(url_for(options))
+      params = ActionController::Routing::Routes.recognize_path(url_string, :method => :get)
+      params[:controller] == @controller.controller_name
+  end
+  
+  def current_action?(options)
+      url_string = CGI.escapeHTML(url_for(options))
+      params = ActionController::Routing::Routes.recognize_path(url_string, :method => :get)
+      params[:controller] == @controller.controller_name && params[:action] == @controller.action_name
+  end
 end
