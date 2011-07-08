9e009d170f9e98c74f5b09faa7aead52670998b8
Christopher Thielen <cthielen@gmail.com>
2011-07-07 21:06:29 -0700

    Pretty up some views. Avoid a bug where the phone number may be missing.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   before_filter CASClient::Frameworks::Rails::Filter
+  
+  #@@user = Person.find_by_loginid(session[:cas_user])
 end
