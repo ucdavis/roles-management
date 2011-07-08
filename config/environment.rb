e92a1451f37d41fa2cdb0487f0e1cfe806052020
Christopher Thielen <cthielen@gmail.com>
2011-07-07 19:04:21 -0700

    Require CAS login for all pages.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+require 'casclient'
+require 'casclient/frameworks/rails/filter'
+
 # Initialize the rails application
 DSSRM::Application.initialize!
+
+CASClient::Frameworks::Rails::Filter.configure(
+  :cas_base_url => "https://cas.ucdavis.edu/cas/"
+)
