869702eded97fe2833275f952ac475418a7b093a
Christopher Thielen <cthielen@gmail.com>
2011-08-10 10:41:38 -0700

    Temporary alteration of validations for LDAP import debug.

--- app/models/person.rb
+++ app/models/person.rb
@@ -11,7 +11,7 @@ class Person < ActiveRecord::Base
   has_many :ou_manager_assignments
   has_many :managements, :through => :ou_manager_assignments, :source => :ou, :primary_key => "manager_id"
   
-  validates_presence_of :first, :last, :loginid
+  validates_presence_of :loginid #:first, :last
   
   def to_param  # overridden
     loginid
