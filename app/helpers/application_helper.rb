a68c05a72a94ca01f7ee25b8bbd95775ec1c8064
Christopher Thielen <cthielen@gmail.com>
2011-08-10 17:35:11 -0700

    Simplify current_controller? to make subURI friendly.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,6 @@
 module ApplicationHelper
   def current_controller?(options)
-      url_string = CGI.escapeHTML(url_for(options))
-      params = ActionController::Routing::Routes.recognize_path(url_string, :method => :get)
-      params[:controller] == @controller.controller_name
-  end
-  
-  def current_action?(options)
-      url_string = CGI.escapeHTML(url_for(options))
-      params = ActionController::Routing::Routes.recognize_path(url_string, :method => :get)
-      params[:controller] == @controller.controller_name && params[:action] == @controller.action_name
+      options[:controller] == @controller.controller_name
   end
   
   def current_user
