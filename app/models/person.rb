7ffdf3fd70693647b677410be3600a90751b245c
Christopher Thielen <cthielen@gmail.com>
2011-08-11 14:12:36 -0700

    Actually enforce ACL via role permissions.

--- app/models/person.rb
+++ app/models/person.rb
@@ -27,6 +27,16 @@ class Person < ActiveRecord::Base
   
   # ACL symbols
   def role_symbols
-    []
+    # Get this app's API key
+    api_key = YAML.load_file("#{Rails.root.to_s}/config/api_keys.yml")['keys']['key']
+    
+    syms = []
+    
+    # Query for permissions of user via API key, converting them into declarative_authentication's needed symbols
+    roles.includes("application").where(:applications => {:api_key => api_key}).each do |role|
+      syms << role.name.underscore.to_sym
+    end
+    
+    syms
   end
 end
