63c572c0a0f6dc96cdd8aeca1006b1497ca9dbff
Christopher Thielen <cthielen@gmail.com>
2011-08-11 15:13:14 -0700

    Include production-mode only Javascript.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+base_uri = "";
+
 $(function() {
-  $("#group_people_tokens").tokenInput("/people.json", {
+  $("#group_people_tokens").tokenInput(base_uri + "/people.json", {
     crossDomain: false,
     prePopulate: $("#group_people_tokens").data("pre"),
     theme: "facebook"
   });
 
-	$("#role_people_tokens").tokenInput("/people.json", {
+	$("#role_people_tokens").tokenInput(base_uri + "/people.json", {
     crossDomain: false,
     prePopulate: $("#role_people_tokens").data("pre"),
     theme: "facebook"
---
+++ public/javascripts/application_production_specific.js
@@ -0,0 +1,5 @@
+// This file will only load in production mode.
+
+// Set this to your applications base URI, if any. Default is "/".
+
+base_uri = "/roles";
