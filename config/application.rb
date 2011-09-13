99f0a5cf9c2a3db2355649e1a0b2b23e97a0ea19
Christopher Thielen <cthielen@gmail.com>
2011-09-12 17:23:15 -0700

    More asset compilation issues.

--- config/application.rb
+++ config/application.rb
@@ -47,6 +47,6 @@ module DSSRM
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.1'
+    config.assets.version = '1.2'
   end
 end
