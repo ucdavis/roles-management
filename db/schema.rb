e0cb46d2ca00d649c4f4fb787add3415ae04cb38
Christopher Thielen <cthielen@gmail.com>
2011-09-19 11:46:57 -0700

    Change CSV to always use double quotes with escaping.

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,8 @@
 
 ActiveRecord::Schema.define(:version => 20110916231520) do
 
-  create_table "affiliations", :id => false, :force => true do |t|
-    t.integer "id"
-    t.string  "name"
+  create_table "affiliations", :force => true do |t|
+    t.string "name"
   end
 
   create_table "application_ou_assignments", :force => true do |t|
