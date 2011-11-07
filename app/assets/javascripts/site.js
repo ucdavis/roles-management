fac83cfd58172c99143f850009f2e96f33ef441e
Christopher Thielen <cthielen@gmail.com>
2011-11-07 10:17:40 -0800

    Drag and drop basics. Needs much styling.

---
+++ app/assets/javascripts/site.js
@@ -0,0 +1,25 @@
+// Not a site-wide JS file - for use in the 'Site' controller
+
+// Set up the drag-and-drop
+$(function() {
+  $( "ul.pins li" ).draggable({
+  	appendTo: "body",
+  	helper: "clone"
+  });
+  $( "ol.pins" ).droppable({
+  	activeClass: "ui-state-default",
+  	hoverClass: "ui-state-hover",
+  	accept: ":not(.ui-sortable-helper)",
+  	drop: function( event, ui ) {
+  		$( this ).find( ".placeholder" ).remove();
+  		$( "<li></li>" ).text( ui.draggable.text() ).appendTo( this );
+  	}
+  }).sortable({
+  	items: "li:not(.placeholder)",
+  	sort: function() {
+  		// gets added unintentionally by droppable interacting with sortable
+  		// using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
+  		$( this ).removeClass( "ui-state-default" );
+  	}
+  });
+});
