568ef00d99709cfe768386da1e4361202d062f0f
Christopher Thielen <cthielen@gmail.com>
2011-11-16 15:37:16 -0800

    Replaced drop target <li>s with divs

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -3,27 +3,28 @@
 // Set up the drag-and-drop
 $(function() {
   $( "ul.pins li" ).draggable({
-  	appendTo: "body",
-  	helper: "clone"
+    appendTo: "body",
+    helper: "clone"
   });
-  $( "ol.pins" ).droppable({
-  	activeClass: "ui-state-default",
-  	hoverClass: "ui-state-hover",
-  	accept: ":not(.ui-sortable-helper)",
-  	drop: function( event, ui ) {
-  	  // Construct the dropped element
-  		$( this ).find( ".placeholder" ).remove();
-  		var el = $( "<li></li>" ).addClass("pin").appendTo( this );
-  		$(el).html( "<img src=\"/images/cancel.png\" style=\"margin: 1px 0 0 0; padding: 0 7px 0 0; float: left; cursor: pointer;\" onClick=\"remove_pin($(this));\" />" + ui.draggable.text());
-  		$("select#s1").dropdownchecklist();
-  	}
+  $( "div.pins" ).droppable({
+    activeClass: "ui-state-default",
+    hoverClass: "ui-state-hover",
+    accept: ":not(.ui-sortable-helper)",
+    drop: function( event, ui ) {
+      // Construct the dropped element
+      $( this ).find( ".placeholder" ).remove();
+      
+      var el = $( "<div></div>" ).addClass("pin").appendTo( this );
+      $(el).html( "<img src=\"/images/cancel.png\" style=\"margin: 1px 0 0 5px; padding: 0 7px 0 0; float: left; cursor: pointer;\" onClick=\"remove_pin($(this));\" /> <a href=\"#\">" + ui.draggable.text() + "</a><div class=\"pin-content\"><p>something</p></div>");
+      $("select#s1").dropdownchecklist();
+    }
   }).sortable({
-  	items: "li:not(.placeholder)",
-  	sort: function() {
-  		// gets added unintentionally by droppable interacting with sortable
-  		// using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
-  		$( this ).removeClass( "ui-state-default" );
-  	}
+    items: "li:not(.placeholder)",
+    sort: function() {
+      // gets added unintentionally by droppable interacting with sortable
+      // using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
+      $( this ).removeClass( "ui-state-default" );
+    }
   });
 });
 
@@ -34,6 +35,6 @@ function remove_pin(el) {
   $(el).parent().remove();
   if(ol.children().length == 0) {
     // Emptied out the last pin. Re-insert the default placerholder text so the 'ol' doesn't disappear entirely
-    ol.append("<li class=\"placeholder\"><img src=\"/assets/add.png\" alt=\"\" /></li>");
+    ol.append("<div class=\"placeholder\"><img src=\"/assets/add.png\" alt=\"\" /></div>");
   }
 }
