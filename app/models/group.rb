3b6066089f029dc9a4b0a4bc337ca85e7a52e25b
Christopher Thielen <cthielen@gmail.com>
2011-12-05 15:53:02 -0800

    Set up nested modelling for group rules

--- app/models/group.rb
+++ app/models/group.rb
@@ -17,6 +17,8 @@ class Group < ActiveRecord::Base
   attr_accessible :name, :people_tokens, :people_ids, :description
   attr_reader :people_tokens
   
+  accepts_nested_attributes_for :rules, :reject_if => lambda { |a| a[:value].blank? || a[:condition].blank? || a[:column].blank? }, :allow_destroy => true
+  
   # Calculates all members (people and groups), including those defined via rules.
   # Uses UID ID numbers (see README)
   def members
--- app/models/group_rule.rb
+++ app/models/group_rule.rb
@@ -2,6 +2,8 @@ class GroupRule < ActiveRecord::Base
   validates_inclusion_of :condition, :in => %w( may\ be may\ not\ be  )
   validates_inclusion_of :column, :in => %w( title major affiliation classification loginid  )
   
+  belongs_to :group
+  
   # Discern the rule and return a UID and name for the person
   def resolve
     u = []
