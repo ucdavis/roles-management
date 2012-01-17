593471fb6d2efc5beeed92baa558566bfac1c36c
Christopher Thielen <cthielen@gmail.com>
2012-01-17 15:43:01 -0800

    jQuery UI organization

--- app/assets/javascripts/administrate.js
+++ app/assets/javascripts/administrate.js
@@ -1,20 +1,28 @@
 $(function() {
   $( "#accordion_0" ).accordion({
     autoHeight: false,
-  	navigation: true
+    active: false,
+  	navigation: true,
+    collapsible: true
   });
   $( "div#accordion_1" ).each(function(i) {
     $(this).accordion({
         autoHeight: false,
-      	navigation: true
+        active: false,
+      	navigation: true,
+        collapsible: true
       });
   });
   $( "div#accordion_2" ).each(function(i) {
     $(this).accordion({
         autoHeight: false,
-      	navigation: true
+        active: false,
+      	navigation: true,
+        collapsible: true,
+        changestart: function(event, ui) {
+          console.log(event);
+          console.log(ui);
+        }
       });
   });
-  
-
 });
