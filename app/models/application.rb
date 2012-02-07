44369e69fd2106db4b036f654ef0677c4cad3c0c
Christopher Thielen <cthielen@gmail.com>
2012-02-07 11:45:52 -0800

    Fixes to application role editing

--- app/models/application.rb
+++ app/models/application.rb
@@ -8,6 +8,8 @@ class Application < ActiveRecord::Base
   attr_accessible :name, :ou_tokens, :ous_ids, :hostname, :display_name, :icon, :description, :ad_path, :roles
   attr_reader :ou_tokens
   
+  accepts_nested_attributes_for :roles, :reject_if => lambda { |a| a[:token].blank? || a[:descriptor].blank? }, :allow_destroy => true
+  
   def ou_tokens=(ids)
       self.ou_ids = ids.split(",")
   end
