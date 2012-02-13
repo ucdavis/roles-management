7a1c60d849fb863bcc33d828a70e6643f48ae037
Christopher Thielen <cthielen@gmail.com>
2012-02-13 12:36:00 -0800

    Security updates

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,9 @@
 class Admin::BaseController < ApplicationController
-  filter_resource_access
+  filter_access_to :all
+  
+  def permission_denied
+    respond_to do |format|
+      format.json { render json: [], status: :unprocessable_entity }
+    end
+  end
 end
--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -1,5 +1,7 @@
 authorization do
   role :admin do
-    has_permission_on [:applications, :groups, :people, :ous, :roles, :classifications], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
+    has_permission_on [:applications, :groups, :people, :ous, :roles, :classifications, :dialogs], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
+    has_permission_on :authorization_rules, :to => :read
+    has_permission_on :admin_dialogs, :to => :impersonate
   end
 end
