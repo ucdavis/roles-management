abd8589616b001e8a98f62555b1f53b4c454c634
Christopher Thielen <cthielen@gmail.com>
2012-02-28 16:34:17 -0800

    Demo autocomplete code injected.

--- app/assets/javascripts/details_modal.js
+++ app/assets/javascripts/details_modal.js
@@ -88,7 +88,40 @@
     // Change the callback accordingly
     switch(mode) {
       case 'loginid':
-        
+      $(this).autocomplete({
+      			source: function( request, response ) {
+      				$.ajax({
+      					url: "http://ws.geonames.org/searchJSON",
+      					dataType: "jsonp",
+      					data: {
+      						featureClass: "P",
+      						style: "full",
+      						maxRows: 12,
+      						name_startsWith: request.term
+      					},
+      					success: function( data ) {
+      						response( $.map( data.geonames, function( item ) {
+      							return {
+      								label: item.name + (item.adminName1 ? ", " + item.adminName1 : "") + ", " + item.countryName,
+      								value: item.name
+      							}
+      						}));
+      					}
+      				});
+      			},
+      			minLength: 2,
+      			select: function( event, ui ) {
+      				console.log( ui.item ?
+      					"Selected: " + ui.item.label :
+      					"Nothing selected, input was " + this.value);
+      			},
+      			open: function() {
+      				$( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+      			},
+      			close: function() {
+      				$( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+      			}
+      		});
         break;
       case 'title':
         
