01d4d0f0b6403ac1c0a5b8fcbf74265eba9b10f4
Christopher Thielen <cthielen@gmail.com>
2012-03-15 18:40:51 -0700

    Templating cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,4 +28,15 @@ module ApplicationHelper
   def allowed_to?(action, controller)
     permitted_to?(action, controller) #unless impersonating? == true
   end
+  
+  # Produces a link_to, adjusting CSS class if link is to current controller
+  def li_link_with_class(options)
+    title = options[:title]
+    options.delete(:title) # to avoid being used by link_to
+    if current_page?(options)
+      ('<li class="active">' + link_to(title, options) + '</li>').html_safe
+    else
+      ('<li>' + link_to(title, options) + '</li>').html_safe
+    end
+  end
 end
