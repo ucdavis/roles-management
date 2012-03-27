e6763472b5d77c6fd186ce82ea5f7bd78fca3d85
Christopher Thielen <cthielen@gmail.com>
2012-03-27 12:04:30 -0700

    Template page work

--- app/models/template_rule.rb
+++ app/models/template_rule.rb
@@ -1,5 +1,5 @@
 class TemplateRule < ActiveRecord::Base
-  validates_inclusion_of :condition, :in => %w( owns\ application has\ unique\ group  )
+  validates_inclusion_of :condition, :in => %w( Owns\ Application Owns\ Unique\ Application Has\ Unique\ Group )
   validates :template_id, :value, :condition, :presence => true
   
   belongs_to :template
