e6c2f0d7306270ffc9a1c9ce9d5d32eb55831dbb
Christopher Thielen <cthielen@gmail.com>
2012-03-27 13:14:55 -0700

    Template filtering

--- app/assets/javascripts/templates.js
+++ app/assets/javascripts/templates.js
@@ -1,3 +1,24 @@
 $(function() {
-  
+  $("#search_templates").change(function() {
+    var value = $(this).val();
+    
+    if(value == "") {
+      $("div.card").show();
+      return;
+    }
+    
+    // Hide all cards
+    $("div.card").hide();
+    
+    // Show only the matching templates
+    $("div.card").each(function(i, o) {
+      if($(o).children("h3").html() == value) {
+        $(o).show();
+      }
+    });
+  });
 });
+
+(function (templates, $, undefined) {
+  templates.templates = null; // will be filled in using the view
+} (window.templates = window.templates || {}, jQuery));
