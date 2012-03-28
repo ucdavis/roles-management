3a28b7d29eaa839f89af66970742e3d2ecae9d3d
Christopher Thielen <cthielen@gmail.com>
2012-03-28 12:27:17 -0700

    Template assignments resolve

--- app/models/template.rb
+++ app/models/template.rb
@@ -2,4 +2,5 @@ class Template < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
   
   has_many :rules, :class_name => "TemplateRule"
+  has_many :assignments, :class_name => "TemplateAssignment"
 end
--- app/models/template_assignment.rb
+++ app/models/template_assignment.rb
@@ -1,11 +1,25 @@
 class TemplateAssignment < ActiveRecord::Base
-  validates :template_id, :value, :condition, :presence => true
+  validates :template_id, :presence => true
+  # Must have either a person_id or group_id
+  validates :person_id, :presence => true, :if => "group_id.nil?"
+  validates :group_id, :presence => true, :if => "person_id.nil?"
   
   belongs_to :template
   belongs_to :person
   belongs_to :group
 
-  def assignment_ids
+  # Returns the UID of this template assignment, regardless of whether it is group- or person-based
+  def resolve
+    uid = nil
     
+    if person_id.nil?
+      # Group-based
+      uid = '2' + group.id.to_s
+    else
+      # Person-based
+      uid = '1' + person.id.to_s
+    end
+    
+    uid
   end
 end
