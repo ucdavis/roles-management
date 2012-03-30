2f095e3ca34c7f6c4622e62aecebc74b4091c124
Christopher Thielen <cthielen@gmail.com>
2012-03-29 19:17:37 -0700

    Removed bad migration

--- db/migrate/20110919185357_drop_affiliation_id_from_person.rb
+++
@@ -1,9 +0,0 @@
-class DropAffiliationIdFromPerson < ActiveRecord::Migration
-  def up
-    remove_column :people, :affiliation_id
-  end
-
-  def down
-    add_column :people, :affiliation_id, :integer
-  end
-end
