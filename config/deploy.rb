10d28b132acd82e559629f03e96c26afff1f3cdc
Christopher Thielen <cthielen@gmail.com>
2012-03-29 18:48:55 -0700

    Deployment changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,52 +1,51 @@
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-require 'rvm/capistrano'
-require 'bundler/capistrano'
+require "bundler/capistrano"
 
-set :whenever_command, "bundle exec whenever"
-require 'whenever/capistrano'
+server "169.237.120.176", :web, :app, :db, primary: true
 
-set :application, "my.dss.ucdavis.edu"
-
-set :repository,  "git://github.com/cthielen/dss-rm.git"
-set :scm, :git
-set :branch, "master"
-
-role :web, application
-role :app, application
-role :db,  application, :primary => true
-
-set :user, "deploy"
-set :deploy_to, "/var/www/apps/dss-rm"
+set :application, "dss-rm"
+set :user, "deployer"
+set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
+set :scm, "git"
+set :repository, "git@github.com:cthielen/#{application}.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
 namespace :deploy do
-  desc "Tell Passenger to restart the app."
-    task :restart do
-      run "touch #{current_path}/tmp/restart.txt"
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
     end
+  end
 
-    desc "Symlink shared configs and folders on each release."
-    task :symlink_shared do
-      run "mkdir -p #{shared_path}/config"
-      run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-      run "ln -nfs #{shared_path}/config/api_keys.yml #{release_path}/config/api_keys.yml"
-      run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
-      
-      # Not sure why we keep having to do this every time ... No way to install system-wide?
-      run "ln -nfs #{shared_path}/vestal_versions #{release_path}/vestal_versions"
-    end
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
 
-    desc "Pre-compile the assets."
-    task :precompile_assets do #, :roles => :web, :except => { :no_release => true } do
-      run "cd #{current_path}; rm -rf public/assets"
-      run "cd #{current_path}; RAILS_ENV=production bundle exec rake assets:precompile"
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
     end
+  end
+  before "deploy", "deploy:check_revision"
 end
-
-after 'deploy:update_code', 'deploy:symlink_shared'
-after 'deploy:update_code', 'deploy:precompile_assets'
-after 'deploy', 'deploy:migrate'
-
-# INSTALLME: Modify this action if you use Capistrano with your own SSH keys
-ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa-deploy")]
