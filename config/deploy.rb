f1c743b9ef2c3c703bb57286e5bb5dae809a7dc9
Christopher Thielen <cthielen@gmail.com>
2012-03-30 12:21:55 -0700

    Auto-run migrations on deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,7 @@ default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
+after "deploy", "deploy:migrations" # run any pending migrations
 
 namespace :deploy do
   %w[start stop restart].each do |command|
