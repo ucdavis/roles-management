fc1b7031b1a8c01dcf8d0e6333b69b8357327ce1
Christopher Thielen <cthielen@gmail.com>
2012-04-19 14:14:36 -0700

    Remove unnecessary exception throwing in JS Routes

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -131,7 +131,7 @@
             delete options[left];
             return this.path_identifier(value); 
           } else {
-            throw new ParameterMissing("Route parameter missing: " + left);
+            //throw new ParameterMissing("Route parameter missing: " + left);
           }
         /*
          * I don't know what are these node types
