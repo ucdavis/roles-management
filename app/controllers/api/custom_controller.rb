851be0caec389341ff1dfe60b2742098d48069b0
Christopher Thielen <cthielen@gmail.com>
2012-04-30 17:31:07 -0700

    Fixed a SQL string concatenation issue. Might cause problems later.

--- app/controllers/api/custom_controller.rb
+++ app/controllers/api/custom_controller.rb
@@ -3,7 +3,7 @@ class Api::CustomController < Api::BaseController
     @results = []
 
     unless params[:q].nil?
-      @people = Person.where("first like ? or last like ?", "%#{params[:q]}%", "%#{params[:q]}%")
+      @people = Person.where("first like ? or last like ? or " + db_concat(:first, ' ', :last) + " like ?", "%#{params[:q]}%", "%#{params[:q]}%", "%#{params[:q]}%")
       @groups = Group.where("name like ?", "%#{params[:q]}%")
     
       @people.each do |person|
@@ -108,5 +108,23 @@ class Api::CustomController < Api::BaseController
     respond_to do |format|
       format.json { render :json => @ous }
     end
+  end
+  
+  private
+  # db_concat is from http://stackoverflow.com/questions/2986405/database-independant-sql-string-concatenation-in-rails
+  # Symbols should be used for field names, everything else will be quoted as a string
+  def db_concat(*args)
+    adapter = ActiveRecord::Base.connection.instance_values["config"][:adapter] #configurations[RAILS_ENV]['adapter'].to_sym
+    args.map!{ |arg| arg.class==Symbol ? arg.to_s : "'#{arg}'" }
+
+    case adapter
+      when :mysql
+        "CONCAT(#{args.join(',')})"
+      when :sqlserver
+        args.join('+')
+      else
+        args.join('||')
+    end
+
   end
 end
