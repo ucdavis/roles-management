967858d596126308562e7a00b58ef4233186313e
Christopher Thielen <cthielen@gmail.com>
2012-09-25 15:06:19 -0700

    Admin permissions update for applications

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -1,11 +1,12 @@
 authorization do
   role :admin do
-    has_permission_on [:applications, :groups, :people, :ous, :roles, :classifications, :dialogs, :templates, :template_assignments], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
+    has_permission_on [:applications, :groups, :people, :ous, :roles, :classifications, :dialogs, :templates, :template_assignments], :to => [:index, :show, :new, :create, :edit, :update, :destroy, :delete]
     has_permission_on :authorization_rules, :to => :read
     has_permission_on :admin_dialogs, :to => [:impersonate, :ip_whitelist]
     has_permission_on :admin_ops, :to => [:impersonate, :unimpersonate]
     has_permission_on :role_assignments, :to => [:create, :delete]
     has_permission_on :admin_ops, :to => [:ad_path_check]
+    has_permission_on :application_owner_assignments, :to => [:create, :delete]
   end
   role :access do
     has_permission_on :groups, :to => [:index, :show, :new, :create, :edit, :update, :destroy]
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,31 +18,31 @@ DSSRM::Application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   # Log e-mails to tmp
-  config.action_mailer.delivery_method = :file
+  #config.action_mailer.delivery_method = :file
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
-  
+
   # Do not compress assets
   config.assets.compress = false
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  
+
   # Raise exception on mass assignment protection for Active Record models
   config.active_record.mass_assignment_sanitizer = :strict
-  
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
-  
+
   config.middleware.use ExceptionNotifier,
     sender_address: 'no-reply@roles.dss.ucdavis.edu',
     exception_recipients: 'cmthielen@ucdavis.edu',
     ignore_exceptions: ExceptionNotifier.default_ignore_exceptions # + [RuntimeError]
-  
-  #config.action_mailer.delivery_method = :file
+
+  config.action_mailer.delivery_method = :letter_opener
 end
