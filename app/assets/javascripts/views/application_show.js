ef55bf2eb2f93c141225886957061acd5c3b1fc8
Christopher Thielen <cthielen@gmail.com>
2012-10-04 22:04:08 -0700

    Moving application details modal to BBJS

---
+++ app/assets/javascripts/views/application_show.js
@@ -0,0 +1,37 @@
+DssRm.Views.ApplicationShow = Support.CompositeView.extend({
+  tagName: "div",
+  className: "",
+
+  events: {
+
+  },
+
+  initialize: function() {
+    _.bindAll(this, "render");
+  },
+
+  render: function () {
+    this.$el.html(JST['applications/show']({ application: this.model }));
+    //this.renderModalContents();
+    return this;
+  },
+
+  renderModalContents: function() {
+    var self = this;
+
+    self.$('h3').html(this.model.escape('name'));
+    self.$('.card-title').attr("title", this.model.escape('description'));
+    self.$('.application-link').attr("href", this.applicationUrl());
+
+    this.model.roles.each(function(role) {
+      var roleItem = new DssRm.Views.RoleItem({ model: role });
+      self.renderChild(roleItem);
+      self.$('.roles').append(roleItem.el);
+    });
+  },
+
+  update: function() {
+    //var complete = this.$('input').prop('checked');
+    this.model.save({ complete: complete });
+  }
+});
