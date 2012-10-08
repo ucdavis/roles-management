91513b3cc93beb9f8a63ce4857b4887fde106977
Christopher Thielen <cthielen@gmail.com>
2012-10-08 14:40:40 -0700

    More application/role BBJS work

--- app/assets/javascripts/views/application_show_role.js
+++ app/assets/javascripts/views/application_show_role.js
@@ -5,6 +5,9 @@ DssRm.Views.ApplicationShowRole = Support.CompositeView.extend({
   render: function () {
     this.$el.html(JST['applications/show_role']({ role: this.model }));
     this.$('input[name=token]').val(this.model.escape('token'));
+    if(this.model.get('default')) this.$('input[name=default]').attr("checked", "checked");
+    this.$('input[name=descriptor]').val(this.model.escape('descriptor'));
+    this.$('input[name=description]').val(this.model.escape('description'));
 
     return this;
   },
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,18 +13,6 @@ module ApplicationHelper
     options[:controller] == controller_name and options[:action] == action_name
   end
 
-  def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + content_tag("button", name, {:type => "button", :class => "btn btn-danger", :onclick => "remove_fields(this)"})
-  end
-
-  def link_to_add_fields(name, f, association)
-    new_object = f.object.class.reflect_on_association(association).klass.new
-    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-      render(association.to_s.singularize + "_fields", :f => builder)
-    end
-    content_tag("button", name, {:type => "button", :class => "btn", :onclick => "add_fields(this, '#{association}', '#{escape_javascript(fields)}')"})
-  end
-
   def _permitted_to?(action_name, controller_name)
     unless current_user == :cas_user_not_in_database
       if impersonating?
