a501c753285d7a85c34d6f56c1bb2b899033e846
Christopher Thielen <cthielen@gmail.com>
2012-12-03 13:43:17 -0800

    Removed unnecessary unit tests

--- test/unit/application_manager_assignment_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ApplicationManagerAssignmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/group_manager_assignment_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GroupManagerAssignmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
