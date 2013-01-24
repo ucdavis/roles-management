97f37901fe3b0cafaa4cb2ca6a8a08d30b2dddd8
Christopher Thielen <cthielen@gmail.com>
2013-01-23 17:33:17 -0800

    Force asset expiration. Asset version now 1.17.

--- config/application.rb
+++ config/application.rb
@@ -48,7 +48,7 @@ module DSSRM
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.16'
+    config.assets.version = '1.17'
 
     config.assets.paths << Rails.root.join("app", "assets", "javascripts", "controllers")
     config.assets.paths << Rails.root.join("app", "assets", "templates")
