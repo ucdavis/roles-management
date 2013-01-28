c0e2feb9a0852fa9ad2a2f4fa026fc9a86a737ef
Christopher Thielen <cthielen@gmail.com>
2013-01-28 13:58:50 -0800

    Added test to ensure valid CAS users with no roles are not allowed access

--- test/functional/applications_controller_test.rb
+++ test/functional/applications_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
+# These tests are run using the fake CAS user 'casuser'
 class ApplicationsControllerTest < ActionController::TestCase
-  test "should get index" do
+  setup do
+    CASClient::Frameworks::Rails::Filter.fake("casuser")
+  end
+
+  test "valid cas user with no roles should get denied" do
     get :index
-    assert_response :success
-    assert_not_nil assigns(:posts)
+    assert Authorization.current_user.role_symbols.length == 0, "current_user should not have had any roles for this test"
+    assert_redirected_to(:controller => "site", :action => "access_denied")
   end
 end
