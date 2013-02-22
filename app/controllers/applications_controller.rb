c3542500fbe004374a8e93638ff2f7b3134bb460
Christopher Thielen <cthielen@gmail.com>
2013-02-22 13:53:17 -0800

    Updates to application controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -99,7 +99,6 @@ class ApplicationsController < ApplicationController
   def load_application
     if permitted_to?(:show, :applications)
       @application = Application.find(params[:id])
-      @application.nil? ? nil : @application = Application.find_by_name(params[:id])
     else
       @application = nil
       logger.info "#{current_user.loginid}@#{request.remote_ip}: Tried loading an application without permission."
