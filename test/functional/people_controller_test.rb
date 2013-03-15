0e24c752cded271ed8d8fe2b3e5cb5fe0bc175df
Christopher Thielen <cthielen@gmail.com>
2013-03-15 13:20:58 -0700

    Cleaned up language on test assertions. Email now required in person JSON output.

--- test/functional/people_controller_test.rb
+++ test/functional/people_controller_test.rb
@@ -7,6 +7,7 @@ class PeopleControllerTest < ActionController::TestCase
   end
 
   # loginid required for: impersonate dialog, group rule "loginid is"
+  # id, name, loginid, email, roles included as per published API spec
   test "JSON request should include certain attributes" do
     grant_test_user_basic_access
 
@@ -14,10 +15,11 @@ class PeopleControllerTest < ActionController::TestCase
 
     body = JSON.parse(response.body)
 
-    assert body.include?('id'), 'JSON response does not include id field'
-    assert body.include?('name'), 'JSON response does not include name field'
-    assert body.include?('type'), 'JSON response does not include type field'
-    assert body.include?('loginid'), 'JSON response does not include loginid field'
+    assert body.include?('id'), 'JSON response should include id field'
+    assert body.include?('name'), 'JSON response should include name field'
+    assert body.include?('email'), 'JSON response should include email field'
+    assert body.include?('type'), 'JSON response should include type field'
+    assert body.include?('loginid'), 'JSON response should include loginid field'
     assert body.include?('roles'), 'JSON response should include roles'
   end
 end
