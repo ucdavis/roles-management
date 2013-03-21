782e3e4e7a8a02b415fb7947932f507c6f103b08
Christopher Thielen <cthielen@gmail.com>
2013-03-21 12:55:26 -0700

    Updated role and application controller tests to ensure members and entities sections exist

--- test/functional/applications_controller_test.rb
+++ test/functional/applications_controller_test.rb
@@ -35,9 +35,15 @@ class ApplicationsControllerTest < ActionController::TestCase
     # body['roles'] should include entities which include name and id (for frontend interface role assignment, applicaiton saving, and new role creation saving (temp ID -> real ID), etc.)
     body['roles'].each do |r|
       assert r["entities"], "JSON response's 'roles' section should include an 'entities' section"
+      assert r["members"], "JSON response's 'roles' section should include a 'members' section"
       r["entities"].each do |e|
         assert e["id"], "JSON response's 'roles' section's 'entities' should include an ID"
         assert e["name"], "JSON response's 'roles' section's 'entities' should include a name"
+        assert e["type"], "JSON response's 'roles' section's 'entities' should include a type"
+      end
+      r["members"].each do |m|
+        assert m["id"], "JSON response's 'roles' section's 'members' should include an ID"
+        assert m["name"], "JSON response's 'roles' section's 'members' should include a name"
       end
     end
   end
