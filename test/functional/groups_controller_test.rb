039bb6c2cfed238791a722b6291b2efcc8f3209a
Christopher Thielen <cthielen@gmail.com>
2013-03-22 14:57:37 -0700

    Added test requirements for Jeremy's Plone scripts

---
+++ test/functional/groups_controller_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+# These tests are run using the fake CAS user 'casuser'
+class GroupsControllerTest < ActionController::TestCase
+  setup do
+    CASClient::Frameworks::Rails::Filter.fake("casuser")
+  end
+
+  test "JSON request should include certain attributes" do
+    grant_test_user_basic_access
+
+    get :show, :format => :json, :id => 2
+
+    body = JSON.parse(response.body)
+
+    assert body.include?('id'), 'JSON response should include id field'
+    assert body.include?('name'), 'JSON response should include name field'
+
+    assert body.include?('owners'), 'JSON response should include owners'
+    body['owners'].each do |o|
+      assert o["id"], "JSON response's 'members' section's 'owners' should include an ID"
+      assert o["name"], "JSON response's 'members' section's 'owners' should include a name"
+      assert o["loginid"], "JSON response's 'members' section's 'owners' should include the login ID"
+    end
+
+    assert body.include?('operators'), 'JSON response should include operators'
+    body['operators'].each do |o|
+      assert o["id"], "JSON response's 'members' section's 'operators' should include an ID"
+      assert o["name"], "JSON response's 'members' section's 'operators' should include a name"
+      assert o["loginid"], "JSON response's 'members' section's 'operators' should include the login ID"
+    end
+    
+    assert body.include?('members'), 'JSON response should include members'
+    body['members'].each do |m|
+      assert m["id"], "JSON response's 'members' section's 'entities' should include an ID"
+      assert m["name"], "JSON response's 'members' section's 'entities' should include a name"
+      assert m["loginid"], "JSON response's 'members' section's 'entities' should include the login ID"
+    end
+    
+  end
+end
--- test/functional/people_controller_test.rb
+++ test/functional/people_controller_test.rb
@@ -8,6 +8,7 @@ class PeopleControllerTest < ActionController::TestCase
 
   # loginid required for: impersonate dialog, group rule "loginid is"
   # id, name, loginid, email, roles included as per published API spec
+  # Should also respond to /people/loginid.json
   test "JSON request should include certain attributes" do
     grant_test_user_basic_access
 
@@ -20,6 +21,14 @@ class PeopleControllerTest < ActionController::TestCase
     assert body.include?('email'), 'JSON response should include email field'
     assert body.include?('type'), 'JSON response should include type field'
     assert body.include?('loginid'), 'JSON response should include loginid field'
+
     assert body.include?('roles'), 'JSON response should include roles'
+    body["roles"].each do |r|
+      assert r["id"], "JSON response's 'roles' section should include an ID"
+      assert r["token"], "JSON response's 'roles' section should include a token"
+      assert r["name"], "JSON response's 'roles' section should include a name"
+      assert r["application_id"], "JSON response's 'roles' section should include an application_id"
+    end
+
   end
 end
