67ab10f8f4a643f08fce15a646dd3165158776ae
Christopher Thielen <cthielen@gmail.com>
2013-04-01 12:33:56 -0700

    Fix for development caching errors

---
+++ config/initializers/load_all_models_in_development.rb
@@ -0,0 +1,8 @@
+# This is NOT THE SAME as cache_classes. cache_classes would make us restart the dev server on any change.
+# This merely loads all models, necessary for Rails.cache to not throw errors.
+# Thanks http://aaronvb.com/articles/37-rails-caching-and-undefined-class-module.
+if Rails.env == "development"
+  Dir.foreach("#{Rails.root}/app/models") do |model_name|
+    require_dependency model_name unless model_name == "." || model_name == ".."
+  end 
+end
