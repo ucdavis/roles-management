b4fca9aa91f3b19f3a317960bbc7942915b6270d
Christopher Thielen <cthielen@gmail.com>
2013-05-17 13:26:53 -0700

    Minor fix to person favorite assignments

--- app/models/person_favorite_assignment.rb
+++ app/models/person_favorite_assignment.rb
@@ -1,10 +1,10 @@
 class PersonFavoriteAssignment < ActiveRecord::Base
   using_access_control
 
-  validates_uniqueness_of :owner_id, :scope => [:person_id, :owner_id]
+  validates_uniqueness_of :owner_id, :scope => [:entity_id, :owner_id]
 
   # favorite is the owner
   belongs_to :owner, :class_name => "Person", :foreign_key => "id"
-  # person is the person being favored
-  belongs_to :entity #, :class_name => "Person"
+  # entity is the entity being favorited
+  belongs_to :entity
 end
