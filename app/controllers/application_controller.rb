bcdeeab38ccb262eaca04cb912b60efae753214c
Christopher Thielen <cthielen@gmail.com>
2013-06-24 16:48:55 -0700

    Try to get browsers to stop caching in development mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :authenticate
+  before_filter :no_cache_for_dev_mode
 
   protected
 
   def permission_denied
     flash[:error] = "Sorry, you are not allowed to access that page."
     redirect_to :controller => 'site', :action => 'access_denied'
+  end
+  
+  def no_cache_for_dev_mode
+    if Rails.env.development?
+      response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+      response.headers["Pragma"] = "no-cache"
+      response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+    end
   end
 end
