4a7f5f67c7fd4b9921c6c605bf9f9daff13b7ade
Christopher Thielen <cthielen@gmail.com>
2013-08-15 12:20:33 -0700

    Removed unnecessary li_link_to_with_class helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,24 +2,11 @@ module ApplicationHelper
   include Authentication
 
   def _has_role?(role)
-    unless current_user == :cas_user_not_in_database
-      if impersonating?
-        # We assume anybody impersonating is an admin and has every role anyway ...
-        yield if block_given?
-      else
-        yield if has_role?(role)
-      end
-    end
-  end
-
-  # Produces a link_to, adjusting CSS class if link is to current controller
-  def li_link_with_class(options)
-    title = options[:title]
-    options.delete(:title) # to avoid being used by link_to
-    if options[:controller] == controller_name and options[:action] == action_name
-      ('<li class="active">' + link_to(title, options) + '</li>').html_safe
+    if impersonating?
+      # We assume anybody impersonating is an admin and has every role anyway ...
+      yield if block_given?
     else
-      ('<li>' + link_to(title, options) + '</li>').html_safe
+      yield if has_role?(role)
     end
   end
 end
