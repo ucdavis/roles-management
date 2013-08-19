2f87ea81731253ef349836a5f1a71ed3261c993e
Christopher Thielen <cthielen@gmail.com>
2013-08-19 14:06:20 -0700

    Performance test for API entities index

--- test/performance/browsing_test.rb
+++ test/performance/browsing_test.rb
@@ -3,7 +3,29 @@ require 'rails/performance_test_help'
 
 # Profiling results for each test method are written to tmp/performance.
 class BrowsingTest < ActionDispatch::PerformanceTest
-  def test_homepage
-    get '/'
+  setup do
+    CASClient::Frameworks::Rails::Filter.fake("casuser")
+    
+    grant_test_user_admin_access
+    
+    generate_entities(1000)
+  end
+  
+  def test_api_entities_index
+    get '/api/entities', :format => :json
+  end
+  
+  # Generates a number of nonsense entities for performance testing
+  def generate_entities(count)
+    without_access_control do
+      for i in 1..count
+        e = Entity.new
+        e.type = "Person"
+        e.loginid = "entity#{i}"
+        e.first = "First#{i}"
+        e.last = "Last#{i}"
+        e.save!
+      end
+    end
   end
 end
