641d3dbb9d08bfb515fa1604046216199132f650
Christopher Thielen <cthielen@gmail.com>
2013-08-20 12:25:25 -0700

    Added API applications#index action and test

---
+++ app/views/api/v1/applications/index.rabl
@@ -0,0 +1,16 @@
+collection @applications
+cache @applications
+
+attributes :id, :name
+
+child :operators => :operators do |operator|
+  attributes :id, :name
+end
+
+child :owners => :owners do |owner|
+  attributes :id, :name
+end
+
+child :roles do |role|
+  attributes :id, :token, :ad_path
+end
--- test/functional/v1/applications_controller_test.rb
+++ test/functional/v1/applications_controller_test.rb
@@ -6,7 +6,42 @@ class Api::V1::ApplicationsControllerTest < ActionController::TestCase
     CASClient::Frameworks::Rails::Filter.fake("casuser")
   end
 
-  test "JSON request should include certain attributes" do
+  test "JSON index request should include certain attributes" do
+    grant_test_user_admin_access
+
+    get :index, :format => :json
+
+    body = JSON.parse(response.body)[0]
+
+    assert body.include?('id'), 'JSON response does not include id field'
+    assert body.include?('name'), 'JSON response does not include name field'
+    refute body.include?('description'), 'JSON response does not include description field'
+    assert body.include?('roles'), 'JSON response should include roles'
+    
+    # body['roles'] should include entities which include name and id (for frontend interface role assignment, applicaiton saving, and new role creation saving (temp ID -> real ID), etc.)
+    body['roles'].each do |r|
+      assert r["id"], "JSON response's 'roles' section should include an ID field"
+      refute r["name"], "JSON response's 'roles' section should not include a name field"
+      assert r["token"], "JSON response's 'roles' section should include a token field"
+      refute r["description"], "JSON response's 'roles' section should not include a description field"
+      # ad_path may be nil so check for existence not value
+      assert r.has_key?("ad_path"), "JSON response's 'roles' section should include an ad_path field"
+    end
+
+    assert body["owners"], "JSON response should include an 'owners' section"
+    body["owners"].each do |o|
+      assert o["id"], "JSON response's 'owners' section's 'members' should include an ID"
+      assert o["name"], "JSON response's 'owners' section's 'members' should include a name"
+    end
+
+    assert body["operators"], "JSON response should include an 'operators' section"    
+    body["operators"].each do |o|
+      assert o["id"], "JSON response's 'roles' section's 'members' should include an ID"
+      assert o["name"], "JSON response's 'roles' section's 'members' should include a name"
+    end
+  end
+
+  test "JSON show request should include certain attributes" do
     grant_test_user_admin_access
 
     get :show, :format => :json, :id => '1'
