e90e23b2e6824e065c4fee411aa5a4afd92cb458
Christopher Thielen <cthielen@gmail.com>
2013-09-18 13:18:32 -0700

    Fix for LDAP priority change

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,7 +8,7 @@ end
 # Run LDAP import updater every hour
 every 6.hours do
   #rake "ldap:import"
-  command 'RAILS_ENV=production nice -n 10 bundle exec rake ldap:import --silent'
+  envcommand 'nice -n 10 bundle exec rake ldap:import --silent'
 end
 
 # Sync with Active Directory every 24 hours at 2:30am (LDAP usually only takes 2-3 mins)
