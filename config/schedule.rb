d69bcabda2d5f8426ac26c6e2c42f8fc7d1d85dc
Christopher Thielen <cthielen@gmail.com>
2013-09-18 13:04:56 -0700

    Reduce LDAP import frequency from 24x / day to 4x / day

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,24 +1,3 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
-
 job_type :envcommand, 'cd :path && RAILS_ENV=:environment :task'
 
 # Ensure our background processor starts up on reboot
@@ -27,7 +6,7 @@ every :reboot do
 end
 
 # Run LDAP import updater every hour
-every 1.hours do
+every 6.hours do
   rake "ldap:import"
 end
 
