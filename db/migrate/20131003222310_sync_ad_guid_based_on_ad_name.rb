3766baadc51f9dcc0300174bf35c575900e0ae5b
Christopher Thielen <cthielen@gmail.com>
2013-10-03 16:16:08 -0700

    Watch for invalid AD paths

--- db/migrate/20131003222310_sync_ad_guid_based_on_ad_name.rb
+++ db/migrate/20131003222310_sync_ad_guid_based_on_ad_name.rb
@@ -6,11 +6,15 @@ class SyncAdGuidBasedOnAdName < ActiveRecord::Migration
     
     Role.where(:ad_path != nil).each do |r|
       g = ActiveDirectoryWrapper.fetch_group(r.ad_path)
+      if g
+        puts "Found group #{g.name}. Saving GUID ..."
       
-      puts "Found group #{g.name}. Saving GUID ..."
-      
-      r.ad_guid = g.objectguid
-      r.save!
+        r.ad_guid = g.objectguid
+        r.save!
+      else
+        puts "Could not find group with AD path #{r.ad_path} on role ID #{r.id}. Ignoring ..."
+        next
+      end
     end
     
     Authorization.ignore_access_control(false)
