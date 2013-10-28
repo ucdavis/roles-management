6dde42b926d816c80d39d0f291ce2db1074b9558
Christopher Thielen <cthielen@gmail.com>
2013-10-28 15:17:57 -0700

    Adjusted roles controller test now that certain information has been excluded

--- test/functional/roles_controller_test.rb
+++ test/functional/roles_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 # These tests are run using the fake CAS user 'casuser'
 class RolesControllerTest < ActionController::TestCase
+  # Used by RM UI when clicking on a role
   test "JSON request should include certain attributes" do
     CASClient::Frameworks::Rails::Filter.fake("casuser")
     
@@ -11,11 +12,12 @@ class RolesControllerTest < ActionController::TestCase
   
     body = JSON.parse(response.body)
   
-    assert body.include?('id'), 'JSON response does not include id field'
-    assert body.include?('token'), 'JSON response does not include token field'
-    assert body.include?('name'), 'JSON response does not include name field'
-    assert body.include?('application_id'), 'JSON response does not include application_id field'
-    assert body.include?('description'), 'JSON response should include description'
+    # assert body.include?('id'), 'JSON response does not include id field'
+    # assert body.include?('token'), 'JSON response does not include token field'
+    # assert body.include?('name'), 'JSON response does not include name field'
+    # assert body.include?('application_id'), 'JSON response does not include application_id field'
+    # assert body.include?('description'), 'JSON response should include description'
+    assert body.include?('member_count'), 'JSON response should include member_count'
     
     assert body.include?('assignments'), 'JSON response should include assignments'
     # body['assignments'] should include certain attributes
