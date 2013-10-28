db3bd6530eb98bb2ce09c627a034ba6d9eddefa0
Christopher Thielen <cthielen@gmail.com>
2013-10-28 09:35:52 -0700

    Use the same applications list cache for admins

--- app/helpers/applications_helper.rb
+++ app/helpers/applications_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationsHelper
   def cache_key_for_applications
-    loginid        = current_user.loginid
+    # Admins share the same application list
+    loginid        = has_role?(:admin) ? 'admin' : 'access-' + current_user.loginid
     count          = Application.count
     max_updated_at = Application.maximum(:updated_at).try(:utc).try(:to_s, :number)
     "#{loginid}/applications/all-#{count}-#{max_updated_at}"
