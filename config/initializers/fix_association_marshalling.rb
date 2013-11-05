86f965de8b8f1ef97765956aec8559c34ba81729
Christopher Thielen <cthielen@gmail.com>
2013-11-05 11:29:46 -0800

    Remove unnecessary initializers. Remove caching bug fixes that are no longer relevant.

--- config/initializers/fix_association_marshalling.rb
+++ config/initializers/fix_association_marshalling.rb
@@ -1,17 +1,16 @@
-module ActiveRecord
-  module Associations
-    class CollectionProxy
-      def respond_to?(name, include_private = false)
-        if super
-          true
-        elsif [:marshal_dump, :_dump].include?(name)
-          false
-        else
-          (load_target && target.respond_to?(name, include_private)) ||
-            proxy_association.klass.respond_to?(name, include_private)
-        end
-      end
-    end
-  end
-end
-
+# module ActiveRecord
+#   module Associations
+#     class CollectionProxy
+#       def respond_to?(name, include_private = false)
+#         if super
+#           true
+#         elsif [:marshal_dump, :_dump].include?(name)
+#           false
+#         else
+#           (load_target && target.respond_to?(name, include_private)) ||
+#             proxy_association.klass.respond_to?(name, include_private)
+#         end
+#       end
+#     end
+#   end
+# end
