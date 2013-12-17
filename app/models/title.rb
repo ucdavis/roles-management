8776c97d79761bf896374c19a318cbcf5709b859
Christopher Thielen <cthielen@gmail.com>
2013-12-17 14:11:54 -0800

    Title names do not have to be unique (they are apparently not)

--- app/models/title.rb
+++ app/models/title.rb
@@ -4,7 +4,7 @@ class Title < ActiveRecord::Base
   has_and_belongs_to_many :classifications
   has_many :people
 
-  validates :name, :presence => true, :uniqueness => true
+  validates :name, :presence => true
   validates :code, :presence => true, :uniqueness => true
 
   def as_json(options={})
