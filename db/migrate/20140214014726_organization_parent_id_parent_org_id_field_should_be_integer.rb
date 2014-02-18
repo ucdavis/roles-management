1118c52967da22fe43d2002ef2a7702a5726fdd4
Christopher Thielen <cthielen@gmail.com>
2014-02-18 11:55:29 -0800

    Update for recent migration to add PostgreSQL compatibility

--- db/migrate/20140214014726_organization_parent_id_parent_org_id_field_should_be_integer.rb
+++ db/migrate/20140214014726_organization_parent_id_parent_org_id_field_should_be_integer.rb
@@ -3,7 +3,7 @@ class OrganizationParentIdParentOrgIdFieldShouldBeInteger < ActiveRecord::Migrat
     include AlterColumn
   end
   
-  def up
+  def self.up
     if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "postgresql"
       # PostgreSQL does not allow us to simply cast a string to an integer even though all column data is compatible
       alter_column :organization_parent_ids, :parent_org_id, :integer, "USING CAST(parent_org_id AS integer)", 1
@@ -12,7 +12,7 @@ class OrganizationParentIdParentOrgIdFieldShouldBeInteger < ActiveRecord::Migrat
     end
   end
 
-  def down
+  def self.down
     change_column :organization_parent_ids, :parent_org_id, :string
   end
 end
