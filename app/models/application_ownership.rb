8d2e7ce94478b3e84bfeed55a5f4511897c6c923
Christopher Thielen <cthielen@gmail.com>
2014-03-03 11:51:10 -0800

    Application ownerships require an application and an entity

--- app/models/application_ownership.rb
+++ app/models/application_ownership.rb
@@ -4,6 +4,7 @@ class ApplicationOwnership < ActiveRecord::Base
   belongs_to :application, :touch => true
   belongs_to :entity, :touch => true
   
+  validates_presence_of :application, :entity
   validates_uniqueness_of :application_id, :scope => [:entity_id, :parent_id]
   
   before_destroy :require_destroy_flag
