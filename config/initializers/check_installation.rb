32545dcfb5efd5eecd560d938801f0c57c73e962
Christopher Thielen <cmthielen@ucdavis.edu>
2014-11-20 12:39:16 -0800

    Corrected the assumption that the database would exist on Rails start. This is not the case when first running rake db:setup.

--- config/initializers/check_installation.rb
+++ config/initializers/check_installation.rb
@@ -1,15 +1,19 @@
 # Some checks to ensure user completed installation successfully as described
 # in README.md.
 
-rm_app = Application.includes(:roles).find_by_name("DSS Roles Management")
+# Application may not exist on 'rake db:setup' so check for ActiveRecord::StatementInvalid
+begin
+  rm_app = Application.includes(:roles).find_by_name("DSS Roles Management")
 
-unless rm_app
-  Authorization.ignore_access_control(true)
+  unless rm_app
+    Authorization.ignore_access_control(true)
 
-  rm_application = Application.create({ name: 'DSS Roles Management', description: 'Manage and organize people with roles.' })
+    rm_application = Application.create({ name: 'DSS Roles Management', description: 'Manage and organize people with roles.' })
 
-  Role.create({ token: 'access', application_id: rm_application.id, name: 'Access', description: 'Allow basic access' })
-  Role.create({ token: 'admin', application_id: rm_application.id, name: 'Admin', description: 'Allow administrator access' })
+    Role.create({ token: 'access', application_id: rm_application.id, name: 'Access', description: 'Allow basic access' })
+    Role.create({ token: 'admin', application_id: rm_application.id, name: 'Admin', description: 'Allow administrator access' })
 
-  Authorization.ignore_access_control(false)
+    Authorization.ignore_access_control(false)
+  end
+rescue ActiveRecord::StatementInvalid => e
 end
