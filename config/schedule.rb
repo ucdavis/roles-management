b83294afa386777557e4fef2dd240d1d116a14e2
Christopher Thielen <cmthielen@ucdavis.edu>
2014-11-20 14:49:20 -0800

    Clean up schedule.rb comments

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,17 +1,17 @@
 job_type :envcommand, 'cd :path && RAILS_ENV=:environment :task'
 
-# Ensure our background processor starts up on reboot
+# Ensure our background processor starts up on reboot (20 processes)
 every :reboot do
-  envcommand 'script/delayed_job restart'
+  envcommand 'script/delayed_job -n 20 restart'
 end
 
-# Run LDAP import updater every 4 hours
-every 4.hours do
+# Run LDAP import updater every 6 hours
+every 6.hours do
   rake "ldap:import"
   #envcommand 'nice -n 10 bundle exec rake ldap:import' # --silent'
 end
 
-# Sync with Active Directory every 24 hours at 2:30am (LDAP usually only takes 2-3 mins)
+# Sync with Active Directory every 4 hours
 every 4.hours do
   rake "ad:sync_all_users"
 end
