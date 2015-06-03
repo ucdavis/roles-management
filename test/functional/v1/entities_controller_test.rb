9fe9a6b9d7d232b2aafe1f24ac9f56632f07aa7f
Christopher Thielen <cmthielen@ucdavis.edu>
2015-06-03 11:20:24 -0700

    Update tests for API JSON changes (added ID and LoginID attributes)

--- test/functional/v1/entities_controller_test.rb
+++ test/functional/v1/entities_controller_test.rb
@@ -4,40 +4,40 @@ require 'test_helper'
 class Api::V1::EntitiesControllerTest < ActionController::TestCase
   test "unauthenticated user should redirected to cas" do
     revoke_access
-    
+
     assert @request.env['REMOTE_ADDR'] != '1.2.3.4'
     # Even if we unset Authorization.current_user, declarative_authorization sets it as "AnonymousUser" with role :guest, so check for that instead of nil
     assert Authorization.current_user.role_symbols.include?(:guest), "current_user should have :guest role for this test"
     assert Authorization.current_user.role_symbols.length == 1, "current_user should not have more than :guest role for this test"
     assert session[:auth_via] != :cas, "authentication should not be set"
-  
+
     # We have to pass 'auth_via' as '' to ensure it is set to an empty string
     # in the session. Deleting session keys doesn't appear to work nor does
     # setting 'auth_via' to nil. I'd love to know why.
     get :index, nil, { 'auth_via' => '' }, nil
-  
+
     assert_response 403
   end
 
   test "valid API whitelisted user should get index" do
     grant_whitelisted_access
-  
+
     get :index, :format => :json
-  
+
     assert_response :success
     assert_not_nil assigns(:entities)
   end
-  
+
   # loginid required for: impersonate dialog, group rule "loginid is"
   # id, name, loginid, email, roles included as per published API spec
   # Should also respond to /people/loginid.json
   test 'JSON index request should include certain attributes' do
     grant_api_user_access
-  
+
     get :index, :format => :json
-  
+
     body = JSON.parse(response.body)[0]
-  
+
     assert body.include?('id'), 'JSON response should include id field'
     assert body.include?('name'), 'JSON response should include name field'
     assert body.include?('type'), 'JSON response should include type field'
@@ -47,53 +47,53 @@ class Api::V1::EntitiesControllerTest < ActionController::TestCase
 
   test 'JSON show request should include certain attributes when entity is a person' do
     grant_api_user_access
-  
+
     get :show, :format => :json, :id => 1
-  
+
     body = JSON.parse(response.body)
-  
-    refute body.include?('id'), 'JSON response should include id field'
+
+    assert body.include?('id'), 'JSON response should include id field'
     assert body.include?('name'), 'JSON response should include name field'
     assert body.include?('type'), 'JSON response should include type field'
     assert body.include?('email'), 'JSON response should include email field'
-    refute body.include?('loginid'), 'JSON response should include loginid field'
-    
+    assert body.include?('loginid'), 'JSON response should include loginid field'
+
     refute body.include?('members'), 'JSON response should not include members'
   end
 
   test 'JSON show request should include certain attributes when entity is a group' do
     grant_api_user_access
-  
+
     get :show, :format => :json, :id => 2
-  
+
     body = JSON.parse(response.body)
-  
-    refute body.include?('id'), 'JSON response should include id field'
+
+    assert body.include?('id'), 'JSON response should include id field'
     assert body.include?('name'), 'JSON response should include name field'
     assert body.include?('type'), 'JSON response should include type field'
-    refute body.include?('loginid'), 'JSON response should include loginid field'
-    
+    refute body.include?('loginid'), 'JSON response should not include loginid field'
+
     assert body.include?('members'), 'JSON response should include members'
     body["members"].each do |m|
       assert m["id"], "JSON response's 'members' section should include an id"
       assert m["type"], "JSON response's 'members' section should include a type"
     end
   end
-  
+
   test "JSON index request should not include inactive entities" do
     grant_api_user_access
-    
+
     get :index, :format => :json
-    
+
     inactiveEntity = entities(:inactivePerson)
-    
+
     entities = JSON.parse(response.body)
-    
+
     entities.each do |e|
       assert e["id"].to_i != inactiveEntity.id, 'JSON response should not include inactive entities'
     end
   end
-  
+
   test "JSON show request should not include inactive entities" do
     grant_api_user_access
 
--- test/functional/v1/roles_controller_test.rb
+++ test/functional/v1/roles_controller_test.rb
@@ -4,17 +4,17 @@ require 'test_helper'
 class Api::V1::RolesControllerTest < ActionController::TestCase
   test "JSON show request should include certain attributes" do
     request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(api_key_users(:apiuser).name, api_key_users(:apiuser).secret)
-    
+
     get :show, :format => :json, :id => '1'
-    
+
     body = JSON.parse(response.body)
-    
-    refute body.include?('id'), 'JSON response does not include id field'
+
+    assert body.include?('id'), 'JSON response does not include id field'
     assert body.include?('token'), 'JSON response does not include token field'
     assert body.include?('name'), 'JSON response does not include name field'
     assert body.include?('application_id'), 'JSON response does not include application_id field'
     assert body.include?('description'), 'JSON response should include description'
-    
+
     assert body.include?('members'), 'JSON response should include members'
     # body['assignments'] should include certain attributes
     body['members'].each do |e|
@@ -23,12 +23,12 @@ class Api::V1::RolesControllerTest < ActionController::TestCase
       assert e['name'], "JSON response 'members' section should include name"
     end
   end
-  
+
   test "JSON show request should not include inactive entities" do
     grant_api_user_access
 
     inactiveEntity = entities(:inactivePerson)
-    
+
     assert inactiveEntity.roles.length > 0, "inactive entity fixture needs at least one role"
 
     get :show, :format => :json, :id => inactiveEntity.roles[0].id
@@ -43,24 +43,24 @@ class Api::V1::RolesControllerTest < ActionController::TestCase
   test "Activating/de-activating a person should invalid role view caches (touch appropriate role and role assignment objects)" do
     without_access_control do
       inactiveEntity = entities(:inactivePerson)
-    
+
       assert inactiveEntity.active == false, "inactive entity should be inactive at the start of the test. Check the fixture."
-    
+
       # The following two lines are equivalent but it doesn't hurt to do have extra testing
       assert inactiveEntity.roles.length > 0, "inactive entity fixture needs at least one role"
       assert inactiveEntity.role_assignments.length > 0, "inactive entity fixture needs at least one role assignment"
-    
+
       # Save the last modified time for an inactive entity's role
       roleAssignment = inactiveEntity.role_assignments.first
       assignedRole = roleAssignment.role
-    
+
       roleAssignment_mtime = roleAssignment.updated_at
       role_mtime = assignedRole.updated_at
-    
+
       # Switch the 'active' status on the entity and ensure the role was updated
       inactiveEntity.active = true
       inactiveEntity.save!
-    
+
       assert inactiveEntity.role_assignments.first.updated_at > roleAssignment_mtime, "role assignment should have been touched due to active/inactive change"
       assert inactiveEntity.role_assignments.first.role.updated_at > role_mtime, "role should have been touched due to active/inactive change"
     end
