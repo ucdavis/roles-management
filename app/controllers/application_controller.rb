f3993d3c53bd7d7b424113c62cc331d543ee5812
Christopher Thielen <cmthielen@ucdavis.edu>
2015-06-17 09:33:10 -0700

    Do not e-mail admins on RecordNotFound exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,12 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate
 
   rescue_from ActiveRecord::RecordNotFound, ActiveRecord::RecordInvalid, ActiveRecord::RecordNotSaved, ActionController::InvalidAuthenticityToken, ActionController::RoutingError do |exception|
-    log_message = "An exception occurred:\n\n#{exception}\n\n" + exception.backtrace.join('\n')
-    logger.error log_message
+    unless exception.class == ActiveRecord::RecordNotFound
+      log_message = "An exception occurred:\n\n#{exception}\n\n" + exception.backtrace.join('\n')
+      logger.error log_message
 
-    AdminMailer.application_error_occurred("dssit-devs-exceptions@ucdavis.edu", log_message).deliver!
+      AdminMailer.application_error_occurred("dssit-devs-exceptions@ucdavis.edu", log_message).deliver!
+    end
 
     raise exception
   end
