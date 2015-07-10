d498c784b4cfa178211bb5eb36bc4688b3ba75fd
Christopher Thielen <cmthielen@ucdavis.edu>
2015-07-10 13:42:35 -0700

    Added Entity.activity to easily fetch an entity's ActivityLogs.

--- app/models/activity_log_tag.rb
+++ app/models/activity_log_tag.rb
@@ -1,5 +1,8 @@
 class ActivityLogTag < ActiveRecord::Base
   using_access_control
-  
+
   attr_accessible :tag
+
+  has_many :activity_log_tag_associations
+  has_many :activity_logs, :through => :activity_log_tag_associations
 end
--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,9 +1,9 @@
 class Entity < ActiveRecord::Base
   using_access_control
-  
+
   has_many :organization_entity_associations
   has_many :organizations, :through => :organization_entity_associations
-  
+
   def person?
     type == 'Person'
   end
@@ -11,10 +11,17 @@ class Entity < ActiveRecord::Base
   def group?
     type == 'Group'
   end
-  
+
   # We need to be able to assign :type when creating an entity using the Entity super-class
   def self.attributes_protected_by_default
     # default is ["id","type"]
     ["id"]
+  end
+
+  # Retrieve 'rows' number of ActivityLog for this entity, if any
+  def activity(rows = 15)
+    tag = ActivityLogTag.find_by_tag("#{self.class.to_s.downcase}_#{self.id}")
+    return nil unless tag
+    return tag.activity_logs.order('performed_at DESC').limit(rows)
   end
 end
