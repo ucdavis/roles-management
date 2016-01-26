2372b19d69bd791698cf782e669fd7761533024d
Christopher Thielen <cmthielen@ucdavis.edu>
2016-01-26 13:02:47 -0800

    Re-enable integration tests, what little we have.

--- test/integration/applications_index_test.rb
+++ test/integration/applications_index_test.rb
@@ -1,44 +1,44 @@
 require 'test_helper'
-#require 'capybara/rails'
-#require 'capybara/poltergeist'
+require 'capybara/rails'
+require 'capybara/poltergeist'
 
-#Capybara.default_driver = :poltergeist
+Capybara.default_driver = :poltergeist
 
 class ApplicationsIndexTest < ActionDispatch::IntegrationTest
-  #include Capybara::DSL
+  include Capybara::DSL
 
   setup do
     CASClient::Frameworks::Rails::Filter.fake("casuser")
   end
 
   test "shows application cards" do
-    # visit('/applications')
-    # current_path == applications_path()
-    #
-    # # Check for the cards-area
-    # assert page.has_selector?('#cards-area'), 'page lacks div#cards-area'
-    #
-    # # Check for the applciation cards 'casuser' has access to
-    # assert page.all(:css, '.card').count == entities(:casuser).manageable_applications.length, 'page should have as many div.card elements as current_user has manageable_applications'
-    #
-    # # page.all('.card').each do |card|
-    # #   puts card.tag_name
-    # #   puts card[:id]
-    # # end
-    #
-    # #print page.html
+    visit('/applications')
+    current_path == applications_path()
+    
+    # Check for the cards-area
+    assert page.has_selector?('#cards-area'), 'page lacks div#cards-area'
+    
+    # Check for the applciation cards 'casuser' has access to
+    assert page.all(:css, '.card').count == entities(:casuser).manageable_applications.length, 'page should have as many div.card elements as current_user has manageable_applications'
+    
+    # page.all('.card').each do |card|
+    #   puts card.tag_name
+    #   puts card[:id]
+    # end
+    
+    # print page.html
   end
 
   test "inactive entities appear in search" do
-    # visit('/applications')
-    # current_path == applications_path()
-    #
-    # # Check for the cards-area
-    # #assert page.has_selector?('#cards-area'), 'page lacks div#cards-area'
-    #
-    # # Check for the applciation cards 'casuser' has access to
-    # #assert page.all(:css, '.card').count == entities(:casuser).manageable_applications.length, 'page should have as many div.card elements as current_user has manageable_applications'
-    #
+    visit('/applications')
+    current_path == applications_path()
+    
+    # Check for the cards-area
+    assert page.has_selector?('#cards-area'), 'page lacks div#cards-area'
+    
+    # Check for the applciation cards 'casuser' has access to
+    assert page.all(:css, '.card').count == entities(:casuser).manageable_applications.length, 'page should have as many div.card elements as current_user has manageable_applications'
+    
     # # page.all('.card').each do |card|
     # #   puts card.tag_name
     # #   puts card[:id]
--- test/integration/group_show_test.rb
+++ test/integration/group_show_test.rb
@@ -2,37 +2,37 @@ require 'test_helper'
 require 'capybara/rails'
 require 'capybara/poltergeist'
 
-#Capybara.default_driver = :poltergeist
+Capybara.default_driver = :poltergeist
 
 class GroupShowTest < ActionDispatch::IntegrationTest
-  #include Capybara::DSL
+  include Capybara::DSL
 
   setup do
     CASClient::Frameworks::Rails::Filter.fake("casuser")
   end
 
   test "ensure non-admins can edit their groups using the UI" do
-    # ## Ensure we can get to the page and open the modal
-    # visit('/applications')
-    # current_path == applications_path()
+    ## Ensure we can get to the page and open the modal
+    visit('/applications')
+    current_path == applications_path()
+    
+    # Click the first sidebar link FIXME: Should select a single one
+    find('#sidebar .entity-details-link').trigger('click')
+    
+    # Modal should appear
+    find('#entityShowModal')
+    
+    # Modal title should be Group's name
+    assert find('.modal-header h3').text == "Group A", "Modal title was expected to be name of group, 'Group A'"
+    
+    # ## Now let's try adding an owner
     #
-    # # # Click the first sidebar link FIXME: Should select a single one
-    # # find('#sidebar .entity-details-link').trigger('click')
-    # #
-    # # # Modal should appear
-    # # find('#entityShowModal')
-    # #
-    # # # Modal title should be Group's name
-    # # assert find('.modal-header h3').text == "Group A", "Modal title was expected to be name of group, 'Group A'"
-    # #
-    # # ## Now let's try adding an owner
-    # #
-    # # # Click the 'Relations' navigation option
-    # # find('.modal-body a[href=\#relations]').trigger('click')
-    #
-    #
-    #
-    # # TODO: Finish me.
+    # # Click the 'Relations' navigation option
+    # find('.modal-body a[href=\#relations]').trigger('click')
+    
+    
+    
+    # TODO: Finish me.
   end
 
   # ensure they can add a favorite and server returns success, re-load model and ensure favorite is there, same for unfavorite
