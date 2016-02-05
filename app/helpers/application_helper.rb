294f361fd74fabda3f8f9f27f99baf756e0d683c
Christopher Thielen <cmthielen@ucdavis.edu>
2016-02-04 18:38:42 -0800

    Replace delcarative_authorization has_role?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,12 @@
 module ApplicationHelper
   include Authentication
 
-  def _has_role?(role)
-    if impersonating?
-      # We assume anybody impersonating is an admin and has every role anyway ...
-      yield if block_given?
-    else
-      yield if has_role?(role)
+  def has_role?(role)
+    if current_user && (current_user.role_symbols.include?(role) || current_user.role_symbols.include?(:admin))
+      return true
     end
+    
+    return false
   end
 
   def _impersonating?
