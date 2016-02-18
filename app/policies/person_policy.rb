016e11f79d5a1ebd06da99856d571ee9e401822d
Christopher Thielen <cmthielen@ucdavis.edu>
2016-02-18 15:42:05 -0800

    Policy update to allow non-admins to create favorites for themselves.

--- app/policies/person_policy.rb
+++ app/policies/person_policy.rb
@@ -1,4 +1,26 @@
 class PersonPolicy < BasePolicy
+  def show?
+    user.has_access?
+  end
+  
+  def update?
+    # A user can update a person ...
+
+    # if they have access and ...
+    if user.has_access?
+      # the person they're updating is themselves ...
+      if user == record
+        # and they're changing nothing about themselves
+        # (this is used when giving yourself a favorite, probably via a .touch())
+        if record.changes.length == 0
+          return true
+        end
+      end
+    end
+    
+    super
+  end
+  
   def search?
     return true if user.is_admin?
     false
