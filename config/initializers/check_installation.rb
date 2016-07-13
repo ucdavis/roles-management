3f71b582f6666c75271d69be6e0f9cdf70e1de16
Christopher Thielen <cmthielen@ucdavis.edu>
2016-07-13 15:17:25 -0700

    Remove declarative_authorization lines from config/initializers/check_installation.rb

--- config/initializers/check_installation.rb
+++ config/initializers/check_installation.rb
@@ -6,14 +6,10 @@ begin
   rm_app = Application.includes(:roles).find_by_name("DSS Roles Management")
 
   unless rm_app
-    Authorization.ignore_access_control(true)
-
-    rm_application = Application.create({ name: 'DSS Roles Management' }) #, description: 'Manage and organize people with roles.' })
+    rm_application = Application.create({ name: 'DSS Roles Management' })
 
     Role.create({ token: 'access', application_id: rm_application.id, name: 'Access', description: 'Allow basic access' })
     Role.create({ token: 'admin', application_id: rm_application.id, name: 'Admin', description: 'Allow administrator access' })
-
-    Authorization.ignore_access_control(false)
   end
 rescue ActiveRecord::StatementInvalid => e
 end
