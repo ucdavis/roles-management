8fb6d3c7fd56963b08b78e895453fea8e8e16bab
Christopher Thielen <cmthielen@ucdavis.edu>
2016-07-13 14:28:44 -0700

    Add authorization policy for Role#update.

--- app/policies/role_policy.rb
+++ app/policies/role_policy.rb
@@ -12,4 +12,18 @@ class RolePolicy < BasePolicy
   
     super
   end
+
+  def update?
+    # A user can update a role ...
+
+    # if they own the role's application
+    return true if record.application.owners.include?(user)
+    
+    # if they operate the role's application
+    return true if record.application.operators.include?(user)
+  
+    return true if user.is_a? ApiWhitelistedIpUser
+
+    super
+  end
 end
