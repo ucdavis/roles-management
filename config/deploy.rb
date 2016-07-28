87644fe124aa49e5f072b8c97177751dd7e92bcf
Christopher Thielen <cmthielen@ucdavis.edu>
2016-07-28 09:02:41 -0700

    Update Capistrano lock to 3.6.0.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.5.0'
+lock '3.6.0'
 
 set :application, 'roles-management'
 set :repo_url, 'https://github.com/dssit/roles-management.git'
