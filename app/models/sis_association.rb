0d8d77dfdc8cd4af6afd9b5cf6f4e31395cc81d9
Christopher Thielen <cmthielen@ucdavis.edu>
2017-11-09 10:22:10 -0800

    Do not validate presence of assocation_rank and level_code so long as data comes from LDAP

--- app/models/sis_association.rb
+++ app/models/sis_association.rb
@@ -2,8 +2,10 @@ class SisAssociation < ApplicationRecord
   belongs_to :entity
   belongs_to :major
 
-  validates_presence_of :association_rank
-  validates_presence_of :level_code
+  # LDAP cannot fill in these fields so we cannot validate their presence.
+  # Once we switch fully from LDAP to IAM, these fields should be validated.
+  # validates_presence_of :association_rank
+  # validates_presence_of :level_code
 
   after_save { GroupRule.resolve_target!(:sis_level_code, entity_id) }
   after_destroy { GroupRule.resolve_target!(:sis_level_code, entity_id) }
