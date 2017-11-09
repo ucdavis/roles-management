e9d53159ecf676a504ac5310c942c0e5fa734e14
Christopher Thielen <cmthielen@ucdavis.edu>
2017-11-09 10:27:09 -0800

    Import PPS departments nightly. Fixes #303.

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,7 +5,11 @@ every :reboot do
   envcommand 'bin/delayed_job -n 10 -p roles restart'
 end
 
+every 24.hours do
+  rake 'dw:import_pps_departments'
+end
+
 # Run LDAP import updater every 6 hours
 every 6.hours do
-  rake "ldap:import"
+  rake 'ldap:import'
 end
