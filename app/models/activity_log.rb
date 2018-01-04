efb51f21ba7d3fcd4084d18290e24d6825c38506
Christopher Thielen <cmthielen@ucdavis.edu>
2018-01-04 14:37:56 -0800

    Use file-based ActivityLogging

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -1,38 +1,43 @@
 class ActivityLog < ApplicationRecord
-  LOG_LEVELS = { :info => 0, :warn => 1, :err => 2 }
+  LOG_LEVELS = { info: 0, warn: 1, err: 2 }.freeze
 
   validates_presence_of :message, :performed_at
-  validates_inclusion_of :level, :in => LOG_LEVELS.values()
+  validates_inclusion_of :level, in: LOG_LEVELS.values
 
   has_many :activity_log_tag_associations
-  has_many :activity_log_tags, :through => :activity_log_tag_associations
+  has_many :activity_log_tags, through: :activity_log_tag_associations
 
   before_validation :set_performed_at_field
 
-  def ActivityLog.record!(message = nil, tags = [], level = :info)
-    al = ActivityLog.create!({message: message, level: LOG_LEVELS[level]})
-
+  def self.record!(message = nil, tags = [], level = :info)
     tags.each do |tag|
-      alt = ActivityLogTag.find_or_create_by(tag: tag)
-      al.activity_log_tags << alt
+      activity_logger = Logger.new(Rails.root.join('log', 'activity').to_s + '/' + tag)
+      activity_logger.formatter = proc do |_severity, _datetime, _progname, msg|
+        "#{msg}\n"
+      end
+      log_level_str = 'INFO' if log.level == 0 # rubocop:disable Style/NumericPredicate
+      log_level_str = 'WARN' if log.level == 1
+      log_level_str = 'ERROR' if log.level == 2
+      activity_logger.info "#{Time.now} - #{log_level_str} - #{message}"
+      activity_logger.close
     end
   end
 
-  def ActivityLog.info!(message = nil, tags = [])
+  def self.info!(message = nil, tags = [])
     ActivityLog.record!(message, tags, :info)
   end
 
-  def ActivityLog.warn!(message = nil, tags = [])
+  def self.warn!(message = nil, tags = [])
     ActivityLog.record!(message, tags, :warn)
   end
 
-  def ActivityLog.err!(message = nil, tags = [])
+  def self.err!(message = nil, tags = [])
     ActivityLog.record!(message, tags, :err)
   end
 
   private
 
   def set_performed_at_field
-    self.performed_at = Time.now unless self.performed_at
+    self.performed_at = Time.now unless performed_at
   end
 end
