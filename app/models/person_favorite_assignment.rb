930827905b18c7727319392a9ea654698a8c9f7f
Christopher Thielen <cmthielen@ucdavis.edu>
2018-01-30 15:33:41 -0800

    Linting fixes

--- app/models/person_favorite_assignment.rb
+++ app/models/person_favorite_assignment.rb
@@ -3,12 +3,12 @@
 # The LDAP import code automatically assigns as a favorite for an OU manager any individuals in that
 # OU.
 class PersonFavoriteAssignment < ApplicationRecord
-  validates_uniqueness_of :owner_id, :scope => [:entity_id, :owner_id]
+  validates_uniqueness_of :owner_id, scope: [:entity_id, :owner_id]
   validates_presence_of :owner
   validates_presence_of :entity
 
   # Owner is the person who holds this favorite
-  belongs_to :owner, :class_name => "Person", :foreign_key => "owner_id", :touch => true
+  belongs_to :owner, class_name: 'Person', foreign_key: 'owner_id', touch: true
   # Entity is the group or person being favorited
-  belongs_to :entity, :touch => true
+  belongs_to :entity, touch: true
 end
