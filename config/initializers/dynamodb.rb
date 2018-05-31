0311322198e22f34eb03f623dd4e1f419db0839c
Christopher Thielen <cmthielen@ucdavis.edu>
2018-05-30 20:22:08 -0700

    Clean up environment variable usage

--- config/initializers/dynamodb.rb
+++ config/initializers/dynamodb.rb
@@ -1,9 +1,13 @@
+region = ENV['DYNAMODB_REGION'] || Rails.application.secrets[:dynamodb_region]
+access_key = ENV['DYNAMODB_AWS_ACCESS_KEY'] || Rails.application.secrets[:dynamodb_aws_access_key]
+secret_key = ENV['DYNAMODB_AWS_SECRET_KEY'] || Rails.application.secrets[:dynamodb_aws_secret_key]
+
 Aws.config.update({
-  region: ENV['DYNAMODB_REGION'] || Rails.application.secrets[:dynamodb_region],
-  credentials: Aws::Credentials.new(ENV['DYNAMODB_AWS_ACCESS_KEY'], ENV['DYNAMODB_AWS_SECRET_KEY'])
+  region: region,
+  credentials: Aws::Credentials.new(access_key, secret_key)
 })
 
-if (ENV['DYNAMODB_REGION'] || Rails.application.secrets[:dynamodb_region]) && ENV['DYNAMODB_AWS_ACCESS_KEY'] && ENV['DYNAMODB_AWS_SECRET_KEY']
+if region && access_key && secret_key
   ::DynamoDbClient = Aws::DynamoDB::Client.new
   ::DynamoDbTable = ENV['DYNAMODB_ACTIVITY_LOG_TABLE'] || Rails.application.secrets[:dynamodb_activity_log_table]
 end
