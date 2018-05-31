284ee721fb10ff284231910baaa0570c962d9787
Christopher Thielen <cmthielen@ucdavis.edu>
2018-05-30 20:22:21 -0700

    Remove file-based activity logs

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -1,32 +1,18 @@
 class ActivityLog
   require 'securerandom'
 
-  LOG_LEVELS = { info: 0, warn: 1, err: 2 }.freeze
+  LOG_LEVELS = { info: 'INFO', warn: 'WARN', err: 'ERROR' }.freeze
 
   def self.record!(message, tags, level = :info)
     return if Rails.env.test?
     return if message.nil?
+    return if tags.nil?
 
     tags.each do |tag|
-      activity_logger = Logger.new(Rails.root.join('log', 'activity').to_s + '/' + tag)
-      activity_logger.formatter = proc do |_severity, _datetime, _progname, msg|
-        "#{msg}\n"
-      end
+      log_level_str = LOG_LEVELS[level]
+      log_level_str ||= "UNKNOWN (#{level})"
 
-      log_level_str = case level
-                      when :info
-                        'INFO'
-                      when :warn
-                        'WARN'
-                      when :err
-                        'ERROR'
-                      else
-                        "UNKNOWN (#{level})"
-                      end
-
-      activity_logger.info "#{Time.now} - #{log_level_str} - #{message}"
       Rails.logger.info "#{tag}: #{Time.now} - #{log_level_str} - #{message}"
-      activity_logger.close
 
       begin
         DynamoDbClient.put_item(
