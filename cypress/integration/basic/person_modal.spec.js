4d379584eed9981fe0ac3b095d3d50c2709c072f
Sadaf Arshad <sarshad@ucdavis.edu>
2018-06-27 10:48:46 -0700

    Add test to check that a person's  modal window is working.

---
+++ cypress/integration/basic/person_modal.spec.js
@@ -0,0 +1,27 @@
+describe('Test that the person show modal can be opened', () => {
+  const bookmarked_person = 'Sadaf Arshad';
+
+  it('Bookmark a person', () => {
+    cy.visit('/applications');
+
+    cy.get('input#search_sidebar.input-large.search-query')
+    .type(bookmarked_person).wait(10)
+    .type('{enter}');
+  });
+
+  it('Clicking a bookmarked person\'s open icon should reveal a modal',() => {
+    cy.get('div#sidebar-area a.entity-details-link:first').click({force: true});
+  });
+
+  it('The modal should have the person\'s name.', () => {
+    cy.get('div.modal-header h3').should('contain', bookmarked_person);
+  });
+
+  it('The modal should have four tabs.', () => {
+    cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(1)').should('contain', 'Summary');
+    cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(2)').should('contain', 'Relations');
+    cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(3)').should('contain', 'Roles');
+    cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(4)').should('contain', 'Activity');
+  });
+
+});
