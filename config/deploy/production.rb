8491bf2016d527ff955686d5aaf7acbacddfe661
Christopher Thielen <cmthielen@ucdavis.edu>
2018-06-28 15:28:27 -0700

    Update VM-hosted RM location

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server 'roles.dss.ucdavis.edu', user: 'deployer', roles: %w{web app db}
+server '169.237.120.176', user: 'deployer', roles: %w{web app db}
 
 
 # role-based syntax
