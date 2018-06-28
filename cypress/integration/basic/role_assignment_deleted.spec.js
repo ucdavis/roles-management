86c8e9138b138071ecd6ae7ac213f41799d91af9
asadaf1 <35669433+asadaf1@users.noreply.github.com>
2018-06-28 10:42:07 -0700

    Create role assignment with delay and wait route.

--- cypress/integration/basic/role_assignment_deleted.spec.js
+++ cypress/integration/basic/role_assignment_deleted.spec.js
@@ -9,31 +9,35 @@ describe('Test that role assignments can be made', () => {
     cy.visit('/applications');
 
     cy.get('input#search_sidebar.input-large.search-query')
-    .type(bookmarked_person).wait(15)
+    .type(bookmarked_person, {delay: 100})
+    .should('have.value', bookmarked_person)
     .type('{enter}');
   });
 
   it('Highlight a role', () => {
-    cy.visit('/applications');
     cy.get('div.role:first a').click({force: true});
   });
 
   it('Click on a favorite that is not assigned to the role',() => {
-    cy.get('ul#pins li.person:first').click().click();
+    cy.server()
+      .route("POST", "/role_assignments").as("roleAssignments")
+      .get('ul#pins li.person:first').click().click()
+      .wait('@roleAssignments');
   });
 
   it('Favorite is moved to that top assigned section', () => {
-    cy.wait(100).get('li.person.highlighted').contains(bookmarked_person);
+    cy.get('li.person.highlighted').contains(bookmarked_person);
   });
 
   it('De-highlight the role', () => {
     cy.get('div.role:first a').click({force: true});
   });
 
-  it('Highlight role again and ensure the newly assigned favorite is still there',() => {
+  it('Highlight again and ensure the newly assigned favorite is still there',() => {
     cy.get('div.role:first a').click({force: true});
     cy.get('li.person.highlighted:last').contains(bookmarked_person);
   });
+
 });
 
 describe('Test that role assignments can be destroyed', () => {
@@ -46,13 +50,11 @@ describe('Test that role assignments can be destroyed', () => {
   it('Click on a role and delete',() => {
     cy.get('li.person.highlighted:last').click().click();
 
-    cy.get('div.modal-header h3').should('contain', 'Are you sure?');
-    cy.get('div.modal-body').contains(bookmarked_person);
-
-    cy.get('div#cards h4#application-name:first').then(($application_name) => {
-      const first_application_name =  $application_name.text();
-
-      cy.get('div.modal-body').contains(first_application_name);
+    cy.get('div.modal-header h3').should('contain', 'Are you sure?')
+      .get('div.modal-body').contains(bookmarked_person)
+      .get('div#cards h4#application-name:first').then(($application_name) => {
+        const first_application_name =  $application_name.text();
+        cy.get('div.modal-body').contains(first_application_name);
     });
 
     cy.get('div.role:first').then(($role_name) => {
