d67c0fae938c3de10a9e2143065483c862d0a3f0
asadaf1 <35669433+asadaf1@users.noreply.github.com>
2018-07-03 11:04:32 -0700

    Ensure that header matches any right sidebar pin.

--- cypress/integration/basic/group_modal.spec.js
+++ cypress/integration/basic/group_modal.spec.js
@@ -1,28 +1,24 @@
-  describe('Group show modal', () => {
-    const BOOKMARKED_GROUP = 'All DSS IT Staff';
-    const KEYBOARD_TYPE_DELAY = 100;
+describe('Group show modal', () => {
+  it('can be opened', () => {
+    cy.visit('/applications');
 
-    it('can be opened',() => {
-      cy.visit('/applications');
-
-      cy.get('input#search_sidebar.input-large.search-query')
-        .type(BOOKMARKED_GROUP, {delay: KEYBOARD_TYPE_DELAY})
-        .should('have.value', BOOKMARKED_GROUP)
-        .type('{enter}');
-
-      cy.get('div#sidebar-area a.entity-details-link:first').click({force: true});
+    beforeEach(function () {
+      cy.get('div#sidebar ul#pins li:first span#name').invoke('text').as('bookmarked_group')
     });
 
-    it('has a header group name', () => {
-      cy.get('div.modal-header h3').should('contain', BOOKMARKED_GROUP);
-    });
-
-    it('has five tabs', () => {
-      cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(1)').should('contain', 'Summary');
-      cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(2)').should('contain', 'Relations');
-      cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(3)').should('contain', 'Roles');
-      cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(4)').should('contain', 'Rules');
-      cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(5)').should('contain', 'Activity');
-    });
-
+    cy.get('div#sidebar-area a.entity-details-link:first').click({force: true});
   });
+
+  it('has a header group name.', function (){
+    cy.get('div.modal-header h3').should('contain', this.bookmarked_group);
+  });
+
+  it('has five tabs.', () => {
+    cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(1)').should('contain', 'Summary');
+    cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(2)').should('contain', 'Relations');
+    cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(3)').should('contain', 'Roles');
+    cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(4)').should('contain', 'Rules');
+    cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(5)').should('contain', 'Activity');
+  });
+
+});
