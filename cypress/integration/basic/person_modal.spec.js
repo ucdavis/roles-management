37de652455027f2d35a987f37216f592e866197b
asadaf1 <35669433+asadaf1@users.noreply.github.com>
2018-07-03 11:01:50 -0700

    Ensure that header matches the right sidebar pin.

--- cypress/integration/basic/person_modal.spec.js
+++ cypress/integration/basic/person_modal.spec.js
@@ -1,24 +1,20 @@
-describe('Test that the person show modal can be opened', () => {
-  const BOOKMARKED_PERSON = 'Sadaf Arshad';
-
-  it('Bookmark a person', () => {
+describe('Person show modal', () => {
+  
+  it('can be opened', () => {
     cy.visit('/applications');
 
-    cy.get('input#search_sidebar.input-large.search-query')
-    .type(BOOKMARKED_PERSON, {delay: 100})
-    .should('have.value', BOOKMARKED_PERSON)
-    .type('{enter}');
-  });
+    beforeEach(function () {
+      cy.get('div#sidebar ul#pins li:first span#name').invoke('text').as('bookmarked_person')
+    });
 
-  it('Clicking a bookmarked person\'s open icon should reveal a modal',() => {
     cy.get('div#sidebar-area a.entity-details-link:first').click({force: true});
   });
 
-  it('The modal should have the person\'s name.', () => {
-    cy.get('div.modal-header h3').should('contain', BOOKMARKED_PERSON);
+  it('has a header group name.', function (){
+    cy.get('div.modal-header h3').should('contain', this.bookmarked_person);
   });
 
-  it('The modal should have four tabs.', () => {
+  it('has four tabs.', () => {
     cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(1)').should('contain', 'Summary');
     cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(2)').should('contain', 'Relations');
     cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(3)').should('contain', 'Roles');
