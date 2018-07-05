daae5c3f31669e206ea4ff0c21b0abdc03da8c9f
asadaf1 <35669433+asadaf1@users.noreply.github.com>
2018-07-05 10:55:17 -0700

    Add member to arbitrary bookmark group.

--- cypress/integration/basic/group_membership.spec.js
+++ cypress/integration/basic/group_membership.spec.js
@@ -1,49 +1,43 @@
-describe('Test that group membership can be edited', () => {
-  const BOOKMARKED_GROUP = 'All DSS IT Staff';
+// FIXME: test assumes a group is already bookmarked.
+
+describe('Group membership tab', () => {
+  const RAILS_COOKIES_NAME = '_DSS-RM_session';
   const NEW_MEMBER = 'Sadaf Arshad';
 
   beforeEach(() => {
-    Cypress.Cookies.preserveOnce('_DSS-RM_session');
+    Cypress.Cookies.preserveOnce(RAILS_COOKIES_NAME);
   });
 
-  it('Open a group modal', () => {
+  it('can be opened', () => {
     cy.visit('/applications');
 
-    cy.get('input#search_sidebar.input-large.search-query')
-    .type(BOOKMARKED_GROUP,{delay: 100})
-    .should('have.value', BOOKMARKED_GROUP)
-    .type('{enter}');
+    beforeEach(function () {
+        cy.get('div#sidebar-area li.group:first span#name').invoke('text').as('bookmarked_group');
+    });
 
-    cy.get('div#sidebar-area a.entity-details-link:first').click({force: true});
-  });
-
-  it('Click the Membership tab',() => {
+    cy.get('div#sidebar-area li.group:first a.entity-details-link:first').click({force: true});
     cy.get('div.modal-body ul.nav.nav-tabs li:nth-child(2) a').click();
   });
 
-  it('Add a member', () => {
+  it('can add a member', function (){
+    cy.get('div.modal-header h3').contains(this.bookmarked_group);
+
     cy.get('p#members ul.token-input-list-facebook li:last input').click()
      .type(NEW_MEMBER, {delay: 500})
      .should('have.value', NEW_MEMBER)
      .type('{enter}');
+
+     cy.get('div.modal-footer button#apply').click();
+     cy.get('p#members').contains(NEW_MEMBER);
   });
 
-  it('Click Apply Changes', () => {
-    cy.get('div.modal-footer button#apply').click();
-  });
-
-  it('Ensure the member is still there',() => {
-    cy.get('p#members').contains(NEW_MEMBER);
-  });
-
-  it('Close the modal and re-open it',() => {
+  it('can be closed',() => {
     cy.get('div.modal-header a').click()
       .get('div#sidebar-area a.entity-details-link:first').click({force: true});
   });
 
-  it('Ensure the member is still there',() => {
+  it('can be opened and new member exits',() => {
     cy.get('div.modal-body ul.nav.nav-tabs li:nth-child(2) a').click()
       .get('p#members').contains(NEW_MEMBER);
   });
-
 });
