f238f3cb31e7bc0c46c25d5ae323f483f6a97af8
asadaf1 <35669433+asadaf1@users.noreply.github.com>
2018-07-05 11:14:32 -0700

    Add FIXME as test assumes a group is bookmarked.

--- cypress/integration/basic/group_modal.spec.js
+++ cypress/integration/basic/group_modal.spec.js
@@ -1,3 +1,5 @@
+// FIXME: test assumes a group is already bookmarked.
+
 describe('Group show modal', () => {
   it('can be opened', () => {
     cy.visit('/applications');
@@ -8,16 +10,15 @@ describe('Group show modal', () => {
     cy.get('div#sidebar-area li.group:first a.entity-details-link:first').click({force: true});
   });
 
-  it('has a header group name.', function (){
+  it('has a header group name', function (){
     cy.get('div.modal-header h3').should('contain', this.bookmarked_group);
   });
 
-  it('has five tabs.', () => {
+  it('has five tabs', () => {
     cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(1)').should('contain', 'Summary');
     cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(2)').should('contain', 'Relations');
     cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(3)').should('contain', 'Roles');
     cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(4)').should('contain', 'Rules');
     cy.get('div.tabbable.tabs-left ul.nav.nav-tabs li:nth-child(5)').should('contain', 'Activity');
   });
-
 });
