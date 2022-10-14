e417dfe64c39612bdaef2315d736e46eda354ace
Jarold Wong <cwjwong@ucdavis.edu>
2022-10-14 12:00:12 -0700

    adding support for docusign user management

---
+++ lib/docusign.rb
@@ -0,0 +1,200 @@
+# One time setup of user consent for jwt user token required -  https://www.docusign.com/blog/developers/oauth-jwt-granting-consent
+# add redirect_uri to App and Keys settings
+# https://account-d.docusign.com/oauth/auth?response_type=code&scope=signature%20impersonation&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}
+
+require "docusign_esign"
+require "Base64"
+
+module Docusign
+  HOST_NAME = ENV["DS_HOST_NAME"]
+  CLIENT_ID = ENV["DS_CLIENT_ID"] # Integration Key
+  USER_ID = ENV["DS_USER_ID"]
+  PRIVATE_KEY = Base64.decode64(ENV["DS_PRIVATE_KEY_64"])
+  EXPIRES_IN = 3600
+
+  API_ACCOUNT_ID = ENV["DS_API_ACCOUNT_ID"]
+
+  # NEW USER DEFAULTS
+  DEFAULT_PERMISSION_PROFILE = 13769603 # "UC Davis Power User"
+  DEFAULT_ADDRESS = {
+    address1: "One Shields Ave",
+    city: "Davis",
+    stateOrProvince: "CA",
+    postalCode: "95776",
+    country: "United States",
+  }
+
+  class DocuSign_eSign::ApiError
+    def detailed_message
+      response_body = JSON.parse(self.response_body)
+      "Error:\n#{self.code} #{self.message}\n#{response_body["errorCode"]}: #{response_body["message"]}"
+    end
+  end
+
+  @configuration = DocuSign_eSign::Configuration.new
+  @configuration.host = HOST_NAME
+
+  @api_client = DocuSign_eSign::ApiClient.new(@configuration)
+
+  # override default for demo account, not required for prod?
+  @api_client.set_oauth_base_path ENV["DS_OAUTH_BASE_PATH"]
+
+  @token = @api_client.request_jwt_user_token(CLIENT_ID, USER_ID, PRIVATE_KEY, EXPIRES_IN)
+  # token = api_client.request_jwt_application_token(CLIENT_ID, PRIVATE_KEY, EXPIRES_IN)
+
+  @api_client.set_default_header("Authorization", "Bearer #{@token.access_token}")
+
+  #   byebug
+  # query for base path from UserInfo
+  #   user_info = @api_client.get_user_info(@token.access_token)
+  #   base_uri = user_info.accounts.first.base_uri
+  #   @api_client.set_base_path(base_uri)
+
+  @users_api = DocuSign_eSign::UsersApi.new(@api_client)
+  @groups_api = DocuSign_eSign::GroupsApi.new(@api_client)
+  @signing_groups_api = DocuSign_eSign::SigningGroupsApi.new(@api_client)
+  @accounts_api = DocuSign_eSign::AccountsApi.new(@api_client) # for permission profiles
+
+  def self.find_user_by_email(email)
+    # API only returns active users by default (not pending/activationSent)
+    # if not found, try again with {status: "ActivationSent"}
+    begin
+      list_options = DocuSign_eSign::ListOptions.new
+      list_options.email = email
+      users_list = @users_api.list(API_ACCOUNT_ID, list_options)
+    rescue DocuSign_eSign::ApiError => e
+      puts e.detailed_message
+
+      puts "User not found found. Retrying for pending users"
+    end
+
+    if users_list.nil?
+      list_options.status = "ActivationSent"
+      begin
+        users_list = @users_api.list(API_ACCOUNT_ID, list_options)
+      rescue => exception
+        puts "User #{email} not found"
+        return nil
+      end
+    end
+
+    return users_list.users.first
+  end
+
+  def self.create_user(name, email)
+    new_users_definition = DocuSign_eSign::NewUsersDefinition.new({
+      newUsers: [{
+        userName: name,
+        email: email,
+        permissionProfileId: DEFAULT_PERMISSION_PROFILE,
+        workAddress: DEFAULT_ADDRESS,
+      }],
+    })
+
+    # limits to 500 users at a time
+    @users_api.create(API_ACCOUNT_ID, new_users_definition).new_users.first
+  end
+
+  def self.find_or_create_user(user)
+    ds_user = find_user_by_email(user[:email])
+
+    if ds_user
+      return ds_user
+    else
+      return self.create_user(user[:name], user[:email])
+    end
+  end
+
+  def self.delete_user(email)
+    user_id = self.find_user_id_by_email(email)
+    user_info_list = DocuSign_eSign::UserInfoList.new({ users: [{ accountId: API_ACCOUNT_ID, userId: user_id }] })
+
+    @users_api.delete(API_ACCOUNT_ID, user_info_list)
+  end
+
+  # Groups (Anthro, Art, Chemistry, etc...), ignore "Everyone" for our purposes
+  def self.get_groups
+    @groups_api.list_groups(API_ACCOUNT_ID).groups.filter { |g| g.group_name != "Everyone" }
+  end
+
+  def self.find_group_by_name(group_name)
+    list_groups_options = DocuSign_eSign::ListGroupsOptions.new
+    list_groups_options.search_text = group_name
+
+    @groups_api.list_groups(API_ACCOUNT_ID, list_groups_options).groups.first
+  end
+
+  # @params [Array<String>] group_names
+  def self.create_groups(group_names)
+    ds_groups = group_names.map { |name| DocuSign_eSign::Group.new({ groupName: name }) }
+    group_information = DocuSign_eSign::GroupInformation.new({ groups: ds_groups })
+
+    @groups_api.create_groups(API_ACCOUNT_ID, group_information)
+  end
+
+  def self.delete_group
+  end
+
+  def self.get_group_users(ds_group)
+    @groups_api.list_group_users(API_ACCOUNT_ID, ds_group.group_id).users
+  end
+
+  def self.add_users_to_group(users, group)
+    user_info_list = DocuSign_eSign::UserInfoList.new({ users: users })
+
+    @groups_api.update_group_users(API_ACCOUNT_ID, group.group_id, user_info_list)
+  end
+
+  def self.remove_users_from_group(users, group)
+    user_info_list = DocuSign_eSign::UserInfoList.new({ users: users })
+
+    @groups_api.delete_group_users(API_ACCOUNT_ID, group.group_id, user_info_list)
+  end
+
+  # Signing Groups (LS VRA Team, Lab, etc...)
+  def self.get_signing_groups
+    @signing_groups_api.list(API_ACCOUNT_ID)
+  end
+
+  # Permission Profiles (Sender, Power User, Admin, etc...)
+  def self.get_permission_profiles
+    @accounts_api.list_permissions(API_ACCOUNT_ID).permission_profiles
+  end
+
+  # Single sync script jobs
+  def self.add_person_to_group(person, group_name)
+    ds_user = self.find_or_create_user({ name: person.name, email: person.email })
+    ds_group = self.find_group_by_name(group_name)
+
+    self.add_users_to_group(Array(ds_user), ds_group)
+  end
+
+  def self.remove_person_from_group(person, group_name)
+    # byebug
+    ds_user = self.find_or_create_user({ name: person.name, email: person.email })
+    ds_group = self.find_group_by_name(group_name)
+
+    self.remove_users_from_group(Array(ds_user), ds_group)
+  end
+
+  # Helper Methods
+  def self.tokenize(group_name)
+    "ds-" + group_name.gsub(/\s/, "-").downcase
+  end
+
+  # Compares DocuSign users and Roles People by email or first+last
+  # returns array of first argument type
+  def self.diff(arr1, arr2)
+    ds_users_first = arr1.first.respond_to? "user_name"
+
+    if ds_users_first
+      arr1.select.each { |ds_user|
+        arr2.none? { |role_member| ds_user.email == role_member.email || "#{role_member.first} #{role_member.last}" == ds_user.user_name }
+      }
+    else
+      arr1.select.each { |role_member|
+        arr2.none? { |ds_user| ds_user.email == role_member.email || "#{role_member.first} #{role_member.last}" == ds_user.user_name }
+      }
+    end
+  end
+end
---
+++ lib/tasks/docusign_tasks.rake
@@ -0,0 +1,179 @@
+require "rake"
+
+namespace :docusign do
+  require "docusign"
+  require "dss_dw"
+
+  desc "Pulls DocuSign groups into RM roles (overwrites existing RM members)"
+  task import: :environment do
+    Rails.logger.info "Running task docusign:import"
+    job_start_ts = Time.now
+
+    ds_application = Application.find_by(name: "DocuSign")
+
+    unless ds_application
+      ds_application = Application.create(name: "DocuSign", description: "A DocuSign group maps to an Application Role with the same name")
+    end
+
+    # Pull in any missing groups
+    ds_groups = Docusign.get_groups
+    rm_roles = ds_application.roles
+
+    ds_group_names = ds_groups.map(&:group_name)
+    rm_role_names = rm_roles.map(&:name)
+
+    group_names_to_add = ds_group_names - rm_role_names
+    groups_to_add = ds_groups.select { |group| group_names_to_add.include? group.group_name }
+
+    groups_to_add.each do |group|
+      token = Docusign.tokenize(group.group_name)
+      new_role = RolesService.create_role(ds_application.id, group.group_name, token, nil, nil)
+
+      ds_group_users = Docusign.get_group_users(group)
+
+      ds_group_users.each do |ds_user|
+        # because some users use login@ucdavis.edu, also search by first and last
+        name = ds_user.user_name.split
+        p = Person.where(email: ds_user.email).or(Person.where(first: name[0], last: name[1])).first
+
+        if p.nil?
+          # check if user exists in IAM and import
+          result = DssDw.search_people(ds_user.email)
+          p = DssDw.create_or_update_using_dw(result[0]["userId"]) if result.size == 1
+          STDOUT.puts "Imported user #{p.loginid} from DW" if p
+
+          # user not in RM or IAM, give up
+          if p.nil?
+            puts "Could not find #{ds_user.email} (#{ds_user.user_name}) for #{new_role.name}. Skipping..."
+            next
+          end
+        end
+
+        # Don't use RoleAssignmentsService here, or trigger Sync system unnecessarily
+        # RoleAssignmentsService.assign_role_to_entity(p, new_role)
+        ra = RoleAssignment.new(role_id: new_role.id, entity_id: p.id)
+        ra.save!
+      end
+    end
+
+    # Delete roles that no longer exists
+    role_names_to_remove = rm_role_names - ds_group_names
+    deleted_roles = []
+
+    role_names_to_remove.each do |role_name|
+      rm_role = rm_roles.select { |r| r.name == role_name }
+      RolesService.destroy_role(rm_role)
+      deleted_roles.push(rm_role)
+    end
+
+    # Sync members of existing groups, skip the ones we just added
+    groups_to_sync = ds_groups - groups_to_add
+
+    groups_to_sync.each do |group|
+      byebug
+      ds_users = Docusign.get_group_users(group)
+      rm_role = rm_roles.find { |role| role.name == group.group_name && role.token == Docusign.tokenize(group.group_name) }
+
+      next if rm_role.nil?
+      role_members = rm_role.members
+
+      users_to_add = Docusign.diff(ds_users, role_members)
+
+      users_to_add.each do |ds_user|
+        name = ds_user.user_name.split
+        p = Person.where(email: ds_user.email).or(Person.where(first: name[0], last: name[1])).first
+
+        if p.nil?
+          # check if user exists in IAM and import
+          # this won't work if user has login@ucdavis.edu for email
+          result = DssDw.search_people(ds_user.email)
+          p = DssDw.create_or_update_using_dw(result[0]["userId"]) if result.size == 1
+          STDOUT.puts "Imported user #{p.loginid} from DW" if p
+
+          # user not in RM or IAM, give up
+          if p.nil?
+            puts "Could not find #{ds_user.email} (#{ds_user.user_name}) for #{new_role.name}. Skipping..."
+            next
+          end
+        end
+
+        # RoleAssignmentsService.assign_role_to_entity(p, rm_role) unless p.roles.include?(rm_role)
+        unless p.roles.include?(rm_role)
+          ra = RoleAssignment.new(role_id: rm_role.id, entity_id: p.id)
+          ra.save!
+        end
+      end
+
+      people_to_remove = Docusign.diff(role_members, ds_users)
+
+      people_to_remove.each do |person|
+        ra = RoleAssignment.find_by(entity_id: person.id, role_id: rm_role.id)
+        ra.destroy!
+      end
+    end
+
+    job_finish_ts = Time.now
+    puts "docusign:import finished in #{job_finish_ts - job_start_ts} seconds"
+    Rails.logger.info "Finished task docusign:import"
+  end
+
+  # overwrites DocuSign group members
+  desc "Pushes Role members to DocuSign groups"
+  task sync: :environment do
+    Rails.logger.info "Running task docusign:sync"
+
+    ds_application = Application.find_by(name: "DocuSign")
+
+    unless ds_application
+      STDERR.puts "Required application 'DocuSign' not found."
+      exit(-1)
+    end
+
+    if ds_application.roles.empty?
+      STDOUT.puts "'DocuSign' application exists but has no roles."
+      exit(0)
+    end
+
+    ds_groups = Docusign.get_groups
+    rm_roles = ds_application.roles
+
+    ds_group_names = ds_groups.map(&:group_name)
+    rm_role_names = ds_application.roles.map(&:name)
+
+    # ensure RM roles exists as DS groups
+    group_names_to_add = rm_role_names - ds_group_names
+    new_ds_groups = Docusign.create_groups(group_names_to_add) unless group_names_to_add.empty?
+
+    # Update membership within each DocuSign group
+    ds_groups += new_ds_groups.groups unless new_ds_groups.nil?
+
+    rm_roles.each do |role|
+      ds_group = ds_groups.find { |dsg| dsg.group_name == role.name }
+
+      ds_group_users = Docusign.get_group_users(ds_group)
+      role_members = role.members
+
+      ds_group_user_emails = ds_group_users.map(&:email)
+      role_member_emails = role.members.map(&:email)
+      member_emails_to_add = role_member_emails - ds_group_user_emails
+
+      ds_users_to_add = member_emails_to_add.map do |email|
+        role_member = role_members.find { |member| member.email == email }
+
+        Docusign.find_or_create_user({ name: role_member.name, email: email })
+      end
+
+      Docusign.add_users_to_group(ds_users_to_add, ds_group) if ds_users_to_add.size > 0
+
+      member_emails_to_remove = ds_group_user_emails - role_member_emails
+
+      ds_users_to_remove = member_emails_to_remove.map do |email|
+        Docusign.find_user_by_email(email)
+      end
+
+      Docusign.remove_users_from_group(ds_users_to_remove, ds_group) if ds_users_to_remove.size > 0
+    end
+
+    Rails.logger.info "Finished task docusign:sync"
+  end
+end
