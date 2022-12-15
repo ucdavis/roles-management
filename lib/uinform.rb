2e2abab818a1fcd233f09bd03724384b3921e609
Jarold Wong <cwjwong@ucdavis.edu>
2022-12-15 13:04:09 -0800

    fix request signature

--- lib/uinform.rb
+++ lib/uinform.rb
@@ -9,27 +9,28 @@ module Uinform
     end
 
     def self.configure
+        @base_url = ENV['UINFORM_BASE_URL']
         @private_key = ENV['UINFORM_PRIVATE_KEY']
         @public_key = ENV['UINFORM_PUBLIC_KEY']
     end
 
     def self.create_signature(method, timestamp)
         raw_signature = "#{method}:#{timestamp}:#{@public_key}"
-        hmac = OpenSSL::HMAC.hexdigest('sha1', @private_key, raw_signature)
-        signature = Base64.encode64(hmac)
+        bytes = OpenSSL::HMAC.digest('sha1', @private_key, raw_signature)
+        signature = Base64.strict_encode64(hmac)
     end
 
     def self.http_get(url)
-        timestamp = Time.now.to_i
-
         uri = URI.parse(url)
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
 
         request = Net::HTTP::Get.new(uri.request_uri)
+
+        timestamp = Time.now.utc.to_i
         request['X-UTIMESTAMP'] = timestamp
-        self.create_signature('GET', timestamp)
-        request.basic_auth public_key, signature
+        signature = self.create_signature('GET', timestamp)
+        request['Authorization'] = "Basic " + Base64::strict_encode64("#{@public_key}:#{signature}")
 
         response = http.request(request)
 
@@ -44,7 +45,9 @@ module Uinform
         return json['responseObject']
     end
 
-    def self.get_user(loginid)
+    def self.get_user_by_login(loginid)
+        endpoint = @base_url + "/AdUsers/sam/" + loginid
+        self.http_get(endpoint)
     end
 
     def self.find_guid_by_group_name(group_name)
